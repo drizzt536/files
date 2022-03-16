e476aed6d3ad86cf8f4522c9a4deea8860f06872
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-03-15 17:54:03 -0700

    Reduce complexity
    Doesn't store hashes into an object and then check the object anymore, but just checks the hashes as it gets to it.

--- Python/hashCracker.py
+++ Python/hashCracker.py
@@ -1,61 +1,40 @@
 import hashlib
 from copy import copy
-def createHash(Input):
-	if not (type(Input) is str or type(Input) is bytes):
-		return
+def create_hash(Input, hashfunction=hashlib.sha256):
 	if type(Input) is str:
-		Input = bytes(Input, "utf-8")
-	h = hashlib.sha256()
-	h.update(Input, "utf-8")
-	a = h.hexdigest()
-	return a
-def crackHashv2(inputHash, debug = False, hashfunction = hashlib.sha256):
+		h = hashfunction()
+		h.update(bytes(Input, "utf-8"))
+		return h.hexdigest()
+
+def hash_cracker_v4(inputHash, debug=False, hashfunction=hashlib.sha256):
 	chars = "default";
 	if debug:
 		chars = input("Characters: ")
-	if not debug or chars == "default":
+	if chars == "default":
 		chars = [
-			"1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
-			"u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X",
-			"Y","Z","`","~","-","_","=","+","(",")","[","]","{","}","\\","|",";",":","'",'"',",","<",".",">","/","?","\t","!","@",
-			"#","$","%","^","&","*"," "
+			b"1", b"2", b"3", b"4", b"5", b"6", b"7", b"8", b"9", b"0",
+			b"a", b"b", b"c", b"d", b"e", b"f", b"g", b"h", b"i", b"j", b"k", b"l", b"m", b"n", b"o", b"p", b"q", b"r", b"s", b"t", b"u", b"v", b"w", b"x", b"y", b"z",
+			b"A", b"B", b"C", b"D", b"E", b"F", b"G", b"H", b"I", b"J", b"K", b"L", b"M", b"N", b"O", b"P", b"Q", b"R", b"S", b"T", b"U", b"V", b"W", b"X", b"Y", b"Z",
+			b"`",b"~", b"-", b"_", b"=", b"+", b"(", b")", b"[", b"]", b"{", b"}", b"\\", b"|", b";", b":", b"'", b'"', b",", b"<", b".", b">", b"/", b"?", b"\t",
+			b"!", b"@", b"#", b"$", b"%", b"^", b"&", b"*", b" "
 		]
 	else:
-		chars = list(set(chars))
-	hasher, obj, List = hashfunction(), {}, copy(chars)
-	#repeat until a match is found
-	
+		chars = list(map(lambda x: bytes(x, "utf-8"), set(chars)))
+	hasher, List, length, numChecked = hashfunction(), copy(chars), 0, 0
+	hasher.update(b"")
+	if inputHash == hasher.hexdigest():
+		return "Result (empty string):"
+	# Repeat until a match is found.
 	while True:
-		#create a hash of all the items in the list and store the hashes as keys in a dictionary with the equivalent string as the values:
-		
-		for i in List:
+		# Check if the original text is already known
+		for s in List:
 			hasher = hashfunction()
-			hasher.update(bytes(i,"utf-8"))
-			obj[f"{hasher.hexdigest()}"] = i
-		#check if the hash is already known:
-		
-		if inputHash in obj:
-			return obj[inputHash]
-		#create the new iteration of the list:
-		
-		for e in range(len(List)):
-			arr = copy(chars)
-			for j in range(len(arr)):
-				arr[j] = List[e] + chars[j]
-			List[e] = arr
-		#remove sub lists
-		while True:
-			b = []
-			for i in List:
-				if type(i) is list:
-					b += i
-				else:
-					b.append(i)
-			List = b
-			for i in List:
-				sublist = False
-				if not (type(i) is list):
-					sublist = True
-					break
-			if sublist:
-				break
+			hasher.update(s)
+			if inputHash == hasher.hexdigest():
+				return "Result:" + str(s)[2:len(s)+2]
+		if debug:
+			print(f"Current Length Checked: {(length := length + 1)},    Combinations Checked: {(numChecked := numChecked + len(List))}")
+		# Extend the list where each item is a list of itself plus each unique character, but don't nest the lists.
+		for i in range(len(List)):
+			List += list(map(lambda x: List[0] + chars[chars.index(x)], chars))
+			List.pop(0)
