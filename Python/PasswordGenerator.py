fa737ad42da102458a1d719399c8ad3bbf1318fe
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-03-26 17:08:02 -0700

    Update PasswordGenerator.py
    fewer lines now. but now it's odd :(

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -1,16 +1,13 @@
-from sys import exit
 from random import randint
 from re import sub
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()`~[]}{|;:',.<>/?-_=+ \"\\"
 passLen, charsRem = abs(int(input("Password Length: "))), input("Characters To Remove: ")
 passw = charsRem2 = ""
-if passLen == 0:
-	passLen = 1
+passLen += not passLen
 for c in charsRem:
-	charsRem2 += (c in "$^()+*\\|[]}{?." and "\\") + f"{c}|"
-chars = sub(f"{charsRem2[:len(charsRem2)-1]}", "", chars)
+	charsRem2 += c in "$^()+*\\|[]}{?." and f"\\{c}|" or f"{c}|"
+chars = sub(charsRem2[:-1], "", chars)
 for i in range(passLen):
 	passw += chars[randint(0, len(chars) - 1)]
 print(f"Password:\n{passw}\n")
-input("Press a Enter to continue...")
-exit(0)
+input("Press 'Enter' to continue...\n")
