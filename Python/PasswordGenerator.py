7b925c14d9a46d92addd5a66bf0f28b57945b954
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-06-21 22:58:35 -0700

    Update PasswordGenerator.py

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -1,5 +1,5 @@
-from random import randint
-from re import sub
+from random import choice
+from re import sub # other printable characters: "\t\r\n\v\f"
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()`~[]}{|;:',.<>/?-_=+ \"\\"
 passLen, charsRem = abs(int(input("Password Length: "))), input("Characters To Remove: ")
 passw = charsRem2 = ""
@@ -8,6 +8,6 @@ for c in charsRem:
 	charsRem2 += c in "$^()+*\\|[]}{?." and f"\\{c}|" or f"{c}|"
 chars = sub(charsRem2[:-1], "", chars)
 for i in range(passLen):
-	passw += chars[randint(0, len(chars) - 1)]
+	passw += choice(chars)
 print(f"Password:\n{passw}\n")
 input("Press 'Enter' to continue...\n")
