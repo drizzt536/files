e830428c0e2eaf6595336a17eab83aa6a5cec5f0
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-07-06 00:25:50 -0700

    Update __init__.py

--- Python/automate/__init__.py
+++ Python/automate/__init__.py
@@ -47,6 +47,8 @@ variables = (
     'movemouse',
     'click',
     'scroll',
+    'vscroll',
+    'hscroll',
     'mousedown',
     'mouseup',
     'releasebuttons',
@@ -105,27 +107,35 @@ def getmouseposition() -> tuple[int, int]:
 
 # Mouse automation
 
-def setmouseposition(x:int=0, y:int=0) -> None:
+def setmouseposition(x: float | int = 0, y: float | int = 0) -> None:
     MouseController()\
     .position = (x, y)
 
-def movemouse(relX:int=0, relY:int=0) -> None:
+def movemouse(relX: float | int = 0, relY: float | int = 0) -> None:
     MouseController()\
     .move(relX, relY)
 
-def click(button:object=Button.left, times:int=1) -> None:
+def click(button: object = Button.left, times: int = 1) -> None:
     MouseController()\
     .click(button, times)
 
-def scroll(rtl:int=0, dtu:int=0) -> None:
+def scroll(ltr: float | int = 0, dtu: float | int = 0) -> None:
     MouseController()\
     .scroll(rtl, dtu)
 
-def mousedown(button:object=Button.left) -> None:
+def vscroll(dtu: float | int = 0) -> None: # vertical scroll
+    MouseController()\
+    .scroll(0, dtu)
+
+def hscroll(ltr: float | int = 0) -> None: # horizontal scroll
+    MouseController()\
+    .scroll(ltr, 0)
+
+def mousedown(button: object = Button.left) -> None:
     buttons.append(button)
     MouseController().press(button)
 
-def mouseup() -> None:
+def mouseup() -> None: # release most recent mouse button
     MouseController()\
     .release( buttons.pop() )
 
@@ -135,35 +145,35 @@ def releasebuttons() -> None:
 
 # Keyboard automation
 
-def keydown(key:object=None) -> None:
+def keydown(key: object = None) -> None:
     if key is None: return
     keys.append(key)
     KeyboardController().press(key)
 
-def keyup() -> None:
+def keyup() -> None: # release most recent key
     KeyboardController()\
     .release( keys.pop() )
 
-def keypress(key:object=None, delay:float=0) -> None:
+def keypress(key: object = None, delay: float | int = 0) -> None:
     if key is None: return
     keydown(key)
     from time import sleep
     sleep(delay)
     keyup()
 
-def typekeys(keys:list|str=None, delay:float=0) -> None:
+def typekeys(keys: list | str | None = None, delay: float | int = 0) -> None:
     if keys is None: return
     for key in keys:
         keypress(key, delay)
 
-def keycombo(keys:list=None, delay:float=0) -> None:
+def keycombo(keys: list | None = None, delay: float | int = 0) -> None:
     if keys is None: return
     for key in keys:
         keydown(key)
     for key in keys:
         keyup()
 
-def alttab(times:int=1) -> None:
+def alttab(times: int = 1) -> None:
     keydown(Key.alt)
     for i in range(int(times)):
         keypress(Key.tab, 0.01)
@@ -179,11 +189,11 @@ def release_all() -> None:
 
 # media stuff
 
-def volumeup(times:int=1) -> None:
+def volumeup(times: int = 1) -> None:
     for i in range(int(times)):
         keypress(Key.media_volume_up)
 
-def volumedown(times:int=1) -> None:
+def volumedown(times: int = 1) -> None:
     for i in range(int(times)):
         keypress(Key.media_volume_down)
 
@@ -208,7 +218,7 @@ def prevmedia() -> None:
     )
 
 if __name__ == '__main__':
-    print("__name__ == '__main__'\n\nvariables:")
+    print("__name__ == '__main__'\nautomate\n\nvariables:")
     print(
         "",
         *variables,
