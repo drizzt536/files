94bc86eb4112c3982ab36755364d0246017084e1
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-07-07 14:14:27 -0700

    Update win logic for rock_paper+scissors.py
    less code now

--- Python/rock_paper_scissors.py
+++ Python/rock_paper_scissors.py
@@ -5,7 +5,7 @@ def rock_paper_scissors() -> None:
     while 1: player = ""
         try: while (player := input("rock, paper, or scissors? ").lower().strip()) not in {"rock", "paper", "scissors", "1", "2", "3"}: continue
         except KeyboardInterrupt: return print("\nfinal score: {player}:{computer}:{tie}\n".format(**scores))
-        computer, player, winner = randint(1, 3), int(player in {"1", "rock"}) or 3 - (player in {"2", "paper"}), player == computer and "tie" or player == computer + 2 and "computer" or (player == computer - 2 or player > computer) and "player" or "computer"
+        computer, player, winner = randint(1, 3), int(player in {"1", "rock"}) or 3 - (player in {"2", "paper"}), player == computer and "tie" or player in {computer - 2, computer + 1} and "player" or "computer"
         scores[winner] += 1
         print("{}{}\nscore: {player}:{computer}:{tie}\n".format(winner, '' if winner == 'tie' else ' wins', **scores))
 
