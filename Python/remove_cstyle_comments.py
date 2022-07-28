448d2c86c28be6ae570bf3032bc7aeee9be1202f
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-07-27 18:16:10 -0700

    Issue fixed from #1
    The functions use iteration instead of regex, which found all instances of // or /*

--- Python/remove_cstyle_comments.py
+++ Python/remove_cstyle_comments.py
@@ -1,15 +1,59 @@
-from automate.misc import read, write
-from re import sub
-# Both functions add an extra newline to the end of the file.
-# This is because I could not figure out how regular expressions work and made it look up until '\n'
-# Instead of line ends. should be $ for line end, but whatever.
-# If there is a singe line comment on the last line of the file, the extra newline doesn't show up.
+def remove_comments_str(text: str, /) -> str:
+	"""takes one argument that is the text for editing
+		does not modify the text because strings are immutable in CPython
+		returns a new string with the new value"""
+	endsWithNewLine = False
+	if not text[-1] == "\n":
+		endsWithNewLine = True
+		text += "\n";
+	stack, inString, removing, output, i, n = [], False, False, "", -1, len(text)
+	while (i := i + 1) < n:
+		if text[i] in {"'", '"', "`"}:
+			if stack:
+				if text[i] == stack[-1]:
+					stack.pop()
+					inString = not inString
+			else:
+				inString = not inString
+				stack.append(text[i])
+		if inString:
+			output += text[i]
+			continue
+		if text[i] == "/":
+			if text[i + 1] == "/":
+				index = i + 2
+				while index < n:
+					if text[index] == "\n":
+						break
+					index += 1
+				i = index
+				output += "\n";
+				continue
+			elif text[i + 1] == "*":
+				insString2 = False
+				index = i - 1
+				stack2 = []
+				while (index := index + 1) < n:
+					inString2 = False
+					if text[index] in {"'", '"', "`"}:
+						if stack2:
+							if text[index] == stack2[-1]:
+								stack2.pop()
+								inString2 = not inString2
+						else:
+							inString2 = not inString2
+							stack2.append(text[index])
+					if not inString2:
+						if text[index] == "*" and text[index + 1] == "/":
+							break
+				i = index + 1
+				continue
+		output += text[i]
+	return output[0:-1] if not endsWithNewLine and text[-1] == "\n" else output
 
-# Takes a file location. removes comments from JavaScript files. edits the file. returns the new file
-def remove_comments_file(loc: str) -> str:
-    write(loc, sub("/\\*(.|\\s)*?\\*/", '', sub("//.*\n?", '\n', read(loc)+'\n')), 'w')
-    return read(loc)
-
-# Takes a file location. removes comments from JavaScript files. does not edit the file. returns the file without comments
-def remove_comments_str(loc: str) -> str:
-    return sub("/\\*(.|\\s)*?\\*/", '', sub("//.*\n?", '\n', read(loc)+'\n'))
+def remove_comments_file(filepath: str | int, edit: bool = False, /) -> str:
+	"""takes the file location of a file. removes all C-style comments from the file.
+		if the second argument is truthy, The file is overwrited to the new state.
+		returns the output string"""
+	from automate.misc import read, write
+	return (edit and write(filepath, (output := remove_comments_str(read(filepath))), 'w')) or output
