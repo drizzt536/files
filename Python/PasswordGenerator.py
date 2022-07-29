c7b5df0bb16d7d7d0af573089f601594b47867de
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-07-29 01:17:44 -0700

    Update PasswordGenerator.py
    uses secrets.choice instead of random.choice, because it is more secure. also now it shuffles the characters beforehand and shuffles the password after creating int.
    if the file is the main file, it also pauses at the end so you can copy the password before the window closes, and uses input for the parameters.

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -1,6 +1,16 @@
-from random import choice, shuffle
-chars = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()`~[]}{|;:',.<>/?-_=+ \"\\")
-length, passw = abs(int(input("Enter password length: "))), ""
-for i in range(length): passw += choice(chars)
-print(f"Password:\n{passw}\n")
-input("Press 'Enter' to continue...\n")
+def password(length: int = 10, shuffles: int = 2) -> str:
+	"""password generator. returns a string"""
+	from secrets import choice
+	from random import shuffle
+	# chars = list(string.printable)
+	chars, psw = list('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'), ""
+	for i in range(shuffles): shuffle(chars)
+	for i in range(length): psw += choice(chars)
+	psw = list(psw)
+	for i in range(shuffles): shuffle(psw)
+	psw = ''.join(psw)
+	print(f"Password:\n{psw}\n")
+	__name__ == "__main__" and input("Press 'Enter' to continue...\n")
+	return psw
+
+__name__ == "__main__" and password( abs(int(input("length: ") or 10)), abs(int(input("shuffles: ") or 2)) )
