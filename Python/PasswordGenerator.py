c97c8b9adbcf7aae885685acb4fd67636ae3633c
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-07-29 01:25:36 -0700

    remove weird chars from PasswordGenerator.py
    remove the characters that are likely to not work for passwords on websites

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -2,8 +2,7 @@ def password(length: int = 10, shuffles: int = 2) -> str:
 	"""password generator. returns a string"""
 	from secrets import choice
 	from random import shuffle
-	# chars = list(string.printable)
-	chars, psw = list('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'), ""
+	chars, psw = list('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '), ""
 	for i in range(shuffles): shuffle(chars)
 	for i in range(length): psw += choice(chars)
 	psw = list(psw)
