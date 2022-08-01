1257688ea4baf944e1c964590a86e627d93fe8b0
drizzt536 <97260963+drizzt536@users.noreply.github.com>
2022-07-31 23:33:43 -0700

    Update PasswordGenerator.py

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -1,26 +1,33 @@
-def password(length: int = 10, shuffles: int = 2) -> str:
+def password(length: int = 10, shuffles: int = 6) -> str:
 	"""password generator. returns a string"""
-	from secrets import choice
-	from random import shuffle
+	from secrets import choice, randbelow
 	from os import system, name as sysname
-	chars = list('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ')
+	def shuffle(s: str, /) -> str:
+		"""kind of like random.shuffle except it works for strings only...
+			and it returns the new value instead of editing the old one.
+			It cant edit the original instance, because strings are imutable in CPython"""
+	    output = ""
+	    while s:
+	        i = randbelow(len(s))
+	        output += s[i]
+	        s = s[:i] + s[i + 1:]
+	    return output
+	chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
 	while 1:
 		psw = ""
-		for i in range(shuffles): shuffle(chars)
+		for i in range(shuffles): chars = shuffle(chars)
 		for i in range(length): psw += choice(chars)
 		if not any(map(lambda x: x in psw, '0123456789')): continue
 		if not any(map(lambda x: x in psw, 'abcdefghijklmnopqrstuvwxyz')): continue
 		if not any(map(lambda x: x in psw, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')): continue
 		if not any(map(lambda x: x in psw, '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')): continue
-		psw = list(psw)
-		for i in range(shuffles): shuffle(psw)
-		psw = ''.join(psw)
+		for i in range(shuffles): psw = shuffle(psw)
 		break
 	if __name__ == "__main__":
 		print(f"Password (in single quotes):\n'{psw}'\n")
 		if input("Again?: ").lower() in {"yes", "y", "1", "true", "t"}:
 			system("cls" if sysname == "nt" else "clear")
-			return password( print() or abs(int(input("length: ") or 10)), abs(int(input("# shuffles: ")) or 2) )
+			return password( print() or abs(int(input("length: ") or 10)), abs(int(input("# shuffles: ")) or 6) )
 	return psw
 
-__name__ == "__main__" and password( print() or abs(int(input("length: ") or 10)), abs(int(input("# shuffles: ")) or 2) )
+__name__ == "__main__" and password( print() or abs(int(input("length: ") or 10)), abs(int(input("# shuffles: ")) or 6) )
