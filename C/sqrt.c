daaee0839320e5df7cc1f6687e4b411357ff0102
Daniel Janusch <drizzt536gaming@gmail.com>
2022-11-16 21:10:27 -0700

    update sqrt.c

--- C/sqrt.c
+++ C/sqrt.c
@@ -1,34 +1,31 @@
-#define printf __builtin_printf
 double atof(const char *_String);
-#define true ((_Bool) +1u)
-#define false ((_Bool) +0u)
-#define DEBUG false
+#define DEBUG_MODE ((_Bool) +0u)
 
 double sqrt(double x) {
 	// √(1/x), 1 / √x
 	if (x < 0) return -1.0f;
 	if (x == 0.0f) return 0.0f;
 
-	double prev1 = x
-		, prev2 = x
-		, start = x;
+	double prev1 = x, prev2 = x, start = x;
+
 	do prev2 = prev1
 		, prev1 = x
 		, x = (prev1 + 1.0f) / (start * prev1 + 1.0f)
-		#if DEBUG
-		, printf(" %.17llf\n", prev1)
-		#endif
+	#if DEBUG_MODE
+		, __builtin_printf(" %.17llf\n", prev1)
+	#endif
 		;
 	while (prev1 != x && x != prev2);
 	return x;
 }
 
 int main(int argc, const char **argv) {
-	if (argc < 2)
-	#if DEBUG
-		return printf("No argument provided")
+	#if DEBUG_MODE
+	if (argc < 2) return __builtin_printf("No argument provided"), 1;
 	#endif
-	;
-	for (int i = 1; i < argc ;) printf("%.18llf\n", sqrt( atof(argv[i++]) ));
+	for (int i = 1; i < argc ;) __builtin_printf(
+		"%.18llf\n",
+		sqrt( atof(argv[i++]) )
+	);
 	return 0;
 }
