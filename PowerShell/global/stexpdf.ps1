55b5232dd5f555df3bfb94ad4be6502ab63c7a93
Daniel Janusch <drizzt536gaming@gmail.com>
2023-01-14 19:09:34 -0700

    update stexpdf

--- PowerShell/global/stexpdf.ps1
+++ PowerShell/global/stexpdf.ps1
@@ -1,11 +1,17 @@
-# add $env:stexpdffile = "%project-root%/PowerShell/global/stexpdf.ps1"
+# $env:stexpdffile = "%project-root%/PowerShell/global/stexpdf.ps1"
 # iex $env:stexpdffile
 # then the function is defined.
+
 function global:stexpdf { # smaller tex pdf compiler
-    param ( $fname )
+    param ( $fname = $null )
+    if ($fname -eq $null) {
+        $fname = (pwd).path
+        $fname = $fname.substring($fname.lastIndexOf("\") + 1)
+    }
     gps | ? name -eq Acrobat | spps
     pdflatex -quiet "./$fname.tex"
     rm "./$fname.aux", "./$fname.log"
     if (test-path "./texput.log") { rm "./texput.log" }
     iex "./$fname.pdf"
+    return $null
 }
