3bac091141cc8a479b0d0461fc2d818ef54661ea
Daniel Janusch <djanusch2007@gmail.com>
2023-02-27 06:43:00 -0700

    update texpdf and stexpdf

--- PowerShell/global/stexpdf.ps1
+++ PowerShell/global/stexpdf.ps1
@@ -8,12 +8,19 @@ function global:stexpdf { # smaller tex pdf compiler
         $fname = (pwd).path
         $fname = $fname.substring($fname.lastIndexOf("\") + 1)
     }
-    gps | ? name -eq Acrobat | spps
+    ps | ? name -eq Acrobat | spps
+    sleep 1
+    if (test-path "./$fname.pdf") { rm "./$fname.pdf" }
     pdflatex -quiet "./$fname.tex"
     pdflatex -quiet "./$fname.tex" # so references work
-    rm "./$fname.aux", "./$fname.log"
-    if (test-path "./texput.log") { rm "./texput.log" }
+    if (test-path "./$fname.log") { rm "./$fname.log" }
+    if (test-path "./$fname.aux") { rm "./$fname.aux" }
     if (test-path "./$fname.toc") { rm "./$fname.toc" }
-    iex "./$fname.pdf"
+    if (test-path "./$fname.out") { rm "./$fname.out" }
+    if (test-path "./texput.log") { rm "./texput.log" }
+    try { iex "./$fname.pdf" } catch {
+        write-host "pdf not created"
+        throw Error
+    }
     return $null
 }
