0945d47637fe60e9a29b227785361a44aeebabbf
Daniel Janusch <djanusch2007@gmail.com>
2023-03-29 00:15:49 -0700

    update existing code and add 2 new versions

--- JavaScript/continuum.js
+++ JavaScript/continuum.js
@@ -1,79 +1,131 @@
 #!/usr/bin/env js
-// continuum.js v1.1 (c) | Copyright 2023 Daniel E. Janusch
+// continuum.js v1.2 (c) | Copyright 2023 Daniel E. Janusch
 
 // This file is licensed by https://raw.githubusercontent.com/drizzt536/files/main/LICENSE
 // and may only be copied IN ITS ENTIRETY under penalty of law.
 
-var generateReals = (function create_realNumbers() {
+var printReals = (function create_printReals() {
+	// all functions assume valid input
 	const isBrowser = (globalThis + "").slice(8, -1).toLowerCase() === "window";
-	if (isBrowser) var process = {
-		stdout: {
-			write: (function create_write() {
-				let str = "";
-				return function write(string = "", flush = false) {
-					str += string.toString();
-					if (flush || str.length > 100) {
-						console.log.call(console, str);
-						str = "";
-					}
-				}
-			})()
-		}
-	}
-	// bitwise shifts are faster than division
-	function R(i, j) { return `${i}.` + `${j}`.split("").reverse().join("") }
-	function triangleNumber(x) { return x * (x + 1n) >> 1n }
+	var write = isBrowser ?
+		(function create_write() {
+
+			var str = "";
+			return function write(string = "", flush = false) {
+				// kind of the same effect as process.stdout.write in node
+
+				str += string;
+				if (flush || str.length > 100)
+					console.log.call(console, str),
+					str = "";
+			}
+
+		})() :
+		str => process.stdout.write.call(process.stdout, str);
+
+	// bit shifts are probably faster than division.
 	function isqrt(n) {
+		// floored (integer) square root.
+		// bigint operations are automatically floored when applicable.
 		if (n < 2n) return n;
-		var x0, x1 = n >> 1n;
 
-		do x0 = x1, x1 = x0 + n / x0 >> 1n;
-		while ( x1 < x0 );
+		var x1 = n >> 1n, x0; // current, previous
 
-		return x0;
+		do [x1, x0] = [x1 + n / x1 >> 1n, x1];
+		while (x1 < x0);
+
+		return x0; // more accurate than `x1`
 	}
-	function greatestTriangleIndex(x) { return x = isqrt(1n + (x << 3n)), ( x + x % 2n >> 1n) - 1n }
-	function generateIndices(x) {
+	function greatestTriangleIndex(x) {
+		// returns the "triangle index" of the greatest triangle number...
+		// less than or equal to the input `x`.
+		// I do not remember how I derived this.
+
+		x = isqrt(1n + (x << 3n)); // some random intermediate value
+		return (x + x % 2n >> 1n) - 1n;
+	}
+	function triangleNumber(n) {
+		// `n` is the "triangle index"
+		return n * (n + 1n) >> 1n;
+	}
+	function generateCoordinates(x) {
+		// converts a 1d index into an appropriate 2d coordinate
 		const u = greatestTriangleIndex(x)
 			, k = triangleNumber(u)
 			, t = x - k;
-		return [t, u - t]; // these can be swapped, it doesn't really matter.
+
+		return [t, u - t];
+		// these can be swapped, but it changes inverse()
+	}
+	function R(i, j) {
+		// `i`: integer part
+		// `j`: reverse of decimal part
+		return `${i}.` + `${j}`.split("").reverse().join("");
 	}
 	function getReal(index) {
-		let positive = true;
-		index % 2n && (positive--, index--);
-		const t = generateIndices(index >> 1n);
-		return (positive ? "" : "-") + R(t[0], t[1]);
+		// generates a unique real number based on the index given.
+
+		return (index % 2n ? "-" : "") + // these two cases can be swapped, but it changes inverse()
+			R( ...generateCoordinates(index >> 1n) );
 	}
-	function generateRealsSet(maxIndex=10n) {
+	function inverse(string) {
+		const match = /^-??(\d+)\.(\d+)$/.exec(string)
+			, i = BigInt(match[1])
+			, j = BigInt(match[2]);
+
+		return (i+j)**2n + 3n*i + j + BigInt(string[0] === "-")
+	}
+
+	function generateRealsList(maxIndex=10n) {
 		try { maxIndex = BigInt(maxIndex) } catch { return [] }
-		for (var i = 0n, set = []; i < maxIndex; i++)
-			set.push( getReal(i) );
+
+		for (var i = 0n, set = []; i < maxIndex ;)
+			set.push( getReal(i++) );
+
 		return set;
 	}
-	function generateReals(maxIndex=1n, delimiter=", ") {
-		// -1n acts as infinity.
+	function printReals(maxIndex=1n, delimiter=", ") {
+		// -1n acts as infinity (or max bigint, in practice)
 		try { maxIndex = BigInt(maxIndex) } catch { return }
-		process.stdout.write("[");
-		for (var i = 0n; i !== maxIndex; ++i) {
-			process.stdout.write( getReal(i) );
-			i + 1n !== maxIndex && process.stdout.write(delimiter);
-		}
-		process.stdout.write("]\n", isBrowser ? true : void 0);
+
+		write("[");
+
+		for (var i = 0n; i !== maxIndex ;)
+			write( getReal(i) ),
+			++i !== maxIndex && write(delimiter);
+
+		write("]\n", true);
 	}
-	return generateReals._isqrt = isqrt,
-		generateReals._greatestTriangleIndex = greatestTriangleIndex,
-		generateReals._triangleNumber = triangleNumber,
-		generateReals._generateIndices = generateIndices,
-		generateReals._R = R,
-		generateReals._getReal = getReal,
-		generateReals._generateRealsSet = generateRealsSet,
-		generateReals;
+
+	return printReals._isqrt = isqrt,
+		printReals._greatestTriangleIndex = greatestTriangleIndex,
+		printReals._triangleNumber = triangleNumber,
+		printReals._generateCoordinates = generateCoordinates,
+		printReals._R = R,
+		printReals.getReal = getReal,
+		printReals.inverse = inverse,
+		printReals.__generateRealsList = generateRealsList,
+		printReals._write = write,
+		printReals;
 })();
 
-function inverse(string) { // assume valid input
-	const match = /^(-?)(\d+)\.(\d)$/.exec(string)
-		, i = BigInt(match[2]) // the x coordinate index, or the integer part
-		, j = BigInt(match[3]); // the y coordinate index, or the decimal part
-	return i*(i+1n) + ((i << 1n) + j)*(j+1n) + BigInt(match[1] === "-");
+
+// short forms using lib.js
+
+function indexToReal(n = 1n) {
+	const a = bMath.sqrt(1n + 4n*n) + 1n >> 1n;
+	const b = n - a*(a - 1n) >> 1n;
+
+	return `${n % 2n ? "-" : ""}${b}.` + `${a - b - 1n}`.reverse();
 }
+function realToIndex(string) {
+	const match = /^-??(\d+)\.(\d+)$/.exec(string);
+	const x = BigInt(match[1]);
+	const y = BigInt(match[2].reverse());
+
+	return (x+y)**2n + 3n*x + y + BigInt(string[0] === "-")
+}
+
+// minified versions using lib.js.
+
+var f=n=>{var a=bMath.sqrt(1n+4n*n)+1n>>1n,b=n-a*(a-1n)>>1n;return`${n%2n?"-":""}${b}.`+`${a-b-1n}`.reverse()},i=s=>{var m=/^-?(\d+)\.(\d+)$/.exec(s),x=BigInt(m[1]),y=BigInt(m[2].reverse());return (x+y)**2n+3n*x+y+BigInt(s[0]=="-")};
