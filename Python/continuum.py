c459686f9c6399f4dcad912073a0bbb040f5b89f
Daniel Janusch <djanusch2007@gmail.com>
2023-05-15 02:20:06 -0700

    change to not use prime()
    the base doesn't actually need to be prime

--- Python/continuum.py
+++ Python/continuum.py
@@ -1,16 +1,3 @@
-def prime(n: int, /) -> int:
-    """
-        - finds the nth prime number via delegation.
-        - incurs around 2 seconds of overhead each call.
-        - calls to wolfram engine because I don't know a fast enough algorithm.
-        - assumes valid input: integer >= 1.
-    """
-    from subprocess import Popen, PIPE
-    process = Popen(["wolframscript", "-c", f"Prime@ {n}"], stdout=PIPE)
-    (output, _error) = process.communicate()
-    process.wait() # idk if this is still needed or not
-    return int( output.decode("utf-8") )
-
 def coprimeQ(a: int, b: int, /) -> bool:
     """
         returns whether or not a and b are coprime, or if gcd(a, b) = 1
@@ -23,20 +10,19 @@ def coprimeQ(a: int, b: int, /) -> bool:
 def main(n: int = 1, m: int = 2, /) -> tuple[str, ...]:
     """
         lists out real numbers in the range (0, 1).
-        uses the nth prime number to the first m negative powers.
+        uses n to the first m negative powers.
     """
     from fractions import Fraction
     if not isinstance(n, int) or not isinstance(m, int):
         raise TypeError("both arguments bust be integers")
     if n < 1 or m < 0:
         raise ValueError("invalid arguments passed. n > 0, m >= 0 (integers)")
-    p = prime(n)
     numbers = []
     for k in range(1, m+1):
-        P = p**k
+        P = n**k
         for i in range(1, P):
             if coprimeQ(P, i):
                 numbers.append(str( Fraction(i, P) ))
     return tuple(numbers)
 
-main(1, 5)
+main(2, 5)
