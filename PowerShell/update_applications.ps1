6d079d13222a27dd23b8749a964d21a0bdec379d
Daniel Janusch <djanusch2007@gmail.com>
2023-06-22 22:02:33 -0700

    use less janky way to find most recent node version

--- PowerShell/update_applications.ps1
+++ PowerShell/update_applications.ps1
@@ -1,3 +1,4 @@
+## Administrator is required for a portion of these updates
 Write-Host "upgrading chocolatey packages"
 choco upgrade all --confirm
 
@@ -9,13 +10,14 @@ winget install --all
 ############ upgrade python ############
 ${python current version} = (python.exe --version) -replace "^Python\s+", ""
 ${python current version} -match "^(\d+)\.(\d+)\.(\d+)$"
-${python current major build} = $Matches[1]
-${python current minor build} = $Matches[2]
+${python current major build} = [int32] $Matches[1]
+${python current minor build} = [int32] $Matches[2]
+# ${python current bugfix}    = [int32] $Matches[3] # not needed
 
-If ( (winget search --source winget --name "Python $(${python current major build} + 1)") -ne 'No package found matching input criteria.' ) {
+If ( (winget search --source winget --name "Python $(1 + ${python current major build})") -ne 'No package found matching input criteria.' ) {
     Write-Host -ForegroundColor Yellow "Python $(1 + ${python current major build}) is released"
 }
-If ( (winget search --source winget --name "Python ${python current major build}.$(1+[int32]${python current minor build})") -ne 'No package found matching input criteria.' ) {
+If ( (winget search --source winget --name "Python ${python current major build}.$(1 + ${python current minor build})") -ne 'No package found matching input criteria.' ) {
     Write-Host -ForegroundColor Yellow "Python ${python current major build}.$(1 + ${python current minor build}) is released"
 }
 (winget search --source winget --name "Python ${python current major build}.${python current minor build}")[-1] -match "(\d+\.\d+\.\d+)\s*$"
@@ -28,6 +30,7 @@ If ($Matches[1] -ne ${python current version}) {
 ############ upgrade python modules ############
 python.exe -m pip install --upgrade pip # pip isn't in pip freeze
 
+# could break some packages
 python.exe -m pip freeze | # list of packages
     %{$_ -replace "^=.+", ""} | # just the name
     %{python.exe -m pip install --upgrade $_}
@@ -35,22 +38,19 @@ python.exe -m pip freeze | # list of packages
 
 
 ############ upgrade nodejs ############
-${most recent nodejs version} = (nvm list available).Split("`n")[3]`
-    -replace "^\|\s+", ""`
-    -replace "\s.+", ""
+${most recent nodejs version} = (
+    Invoke-RestMethod -Uri 'https://nodejs.org/dist/index.json'`
+    -Method GET
+)[0].Version # vX.Y.Z
 
-${nodejs active version} = (nvm list | ?{ $_.indexOf("*") -ne -1 })`
-    -replace "^\s+\*\s+", ""`
-    -replace "\s+\(.+$", ""
+${nodejs current version} = nvm current
 
-If (${most recent nodejs version} -ne ${nodejs active version}) {
+If (${most recent nodejs version} -ne ${nodejs current version}) {
     nvm install ${most recent nodejs version}
     nvm use ${most recent nodejs version}
-    nvm uninstall ${nodejs active version}
+    nvm uninstall ${nodejs current version}
 
-    (new-object -c shell.application).
-        Namespace("$env:ProgramFiles\nodejs").
-        ParseName("node.exe").InvokeVerb("open")
+    Start-Process node.exe
     ## user should switch which version of node is pinned to the taskbar
 }
 
