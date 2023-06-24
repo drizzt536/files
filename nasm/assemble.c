5a96a5c756996307b1ff17823b9fdd61c3977377
Daniel Janusch <djanusch2007@gmail.com>
2023-06-23 18:06:30 -0700

    rewrite assemble.ps1 in C
    now it is easier to use. I don't actually know how to load functions from powershell files, but I do know how to run executables.

---
+++ nasm/assemble.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	// nasm -fwin64 -Werror "./$name.$extn" $params
+	// gcc "./$name.obj" -o $name
+	// rm "./$name.obj"
+	// iex "./$name.exe"
+
+	// argv[0]: path to this file
+	// argv[1]: file name    ; argc > 1 ==> name   included
+	// argv[2]: extension    ; argc > 2 ==> extn   included
+	// argv[3]: extra params ; argc > 3 ==> params included
+
+	if (argc == 1) {
+		puts("no arguments provided");
+		return 1;
+	}
+	
+	char *_extn = argc > 2 ? argv[2] : ".nasm";
+	char extn[1 + strlen(_extn)];
+	sprintf(extn, *_extn == '.' ? "%s" : ".%s", _extn);
+
+	char name[2 + strlen(argv[1])];
+	sprintf(name, "./%s", argv[1]);
+
+	char *params = argc > 3 ? argv[3] : "";
+	int name_length = strlen(name);
+
+	{
+		char cmd[22 + name_length + strlen(extn) + strlen(params)];
+		sprintf(cmd, "nasm -fwin64 -Werror %s%s %s", name, extn, params);
+		printf("running nasm: %s\n", cmd);
+		system(cmd);
+	}
+	{
+		char cmd[12 + 2*name_length];
+		sprintf(cmd, "gcc %s.obj -o %s", name, name);
+		printf("linking with gcc: %s\n", cmd);
+		system(cmd);
+	}
+	char cmd[2 + name_length];
+	sprintf(cmd, "%s.obj", name);
+	printf("removing intermediate %s\n", cmd);
+	remove(cmd);
+
+	return 0;
+}
