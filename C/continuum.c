db4d7ebbe5b5e279a0e9ee2589d31bb9607239ac
Daniel Janusch <djanusch2007@gmail.com>
2023-07-06 02:16:52 -0700

    replace ullong with uLong, make DELIMITER a preprocessor constant.

--- C/continuum.c
+++ C/continuum.c
@@ -1,4 +1,4 @@
-// continuum.c v1.0 (c) | Copyright 2023 Daniel E. Janusch
+// continuum.c v1.1 (c) | Copyright 2023 Daniel E. Janusch
 
 // This file is licensed by https://raw.githubusercontent.com/drizzt536/files/main/LICENSE
 // and may only be copied IN ITS ENTIRETY under penalty of law.
@@ -6,19 +6,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ullong unsigned long long int
+#define uLong unsigned long long int
+#define DELIMITER ", "
 
-const char *DELIMITER = ", ";
 
+static uLong sgn(uLong x) { return (uLong) (x < 0ull ? -1ull : x > 0ull); }
 
-static ullong sgn(ullong x) { return (ullong) (x < 0ull ? -1ull : x > 0ull); }
+static uLong triangleNumber(uLong x) { return x * (x + 1ull) / 2ull; }
 
-static ullong triangleNumber(ullong x) { return x * (x + 1ull) / 2ull; }
-
-static ullong isqrt(ullong n) {
+static uLong isqrt(uLong n) {
 	if (n < 2ull) return n;
 
-	ullong n_0, n_1 = n / 2ull;
+	uLong n_0, n_1 = n / 2ull;
 
 	do n_0 = n_1,
 		n_1 = ( n_0 + n / n_0 ) / 2ull;
@@ -27,28 +26,28 @@ static ullong isqrt(ullong n) {
 	return n_0;
 }
 
-static ullong greatestTriangleIndex(ullong x) {
+static uLong greatestTriangleIndex(uLong x) {
 	return (
 		isqrt(1ull + 8ull*x) +
 		sgn( 1ull - sgn(1ull - isqrt(1ull + 8ull*x) % 2ull) )
 	) / 2ull - 1ull;
 }
 
-static ullong *generateIndices(ullong x) {
-	ullong u = greatestTriangleIndex(x)
+static uLong *generateIndices(uLong x) {
+	uLong u = greatestTriangleIndex(x)
 		, k = triangleNumber(u)
 		, t = x - k;
-	ullong *arr = (ullong *) malloc( 2*sizeof(ullong) );
+	uLong *arr = (uLong *) malloc( 2*sizeof(uLong) );
 	arr[0] = t;
 	arr[1] = u - t;
 	return arr;
 }
 
-static ullong reverse(ullong x) {
-	ullong ans = 0ull;
+static uLong reverse(uLong x) {
+	uLong ans = 0ull;
 	 
 	while (x > 0ull) {
-		ullong tmp = x % 10ull;
+		uLong tmp = x % 10ull;
 		ans *= 10ull;
 		ans += tmp;
 
@@ -58,45 +57,45 @@ static ullong reverse(ullong x) {
 	return ans;
 }
 
-static ullong len(ullong x) {
-	ullong length = 0;
+static uLong len(uLong x) {
+	uLong length = 0;
 	do x /= 10ull, length++;
 	while (x > 0ull);
 	return length;
 }
 
-static ullong ullpow10(ullong x) {
-	ullong ans = 1ull;
+static uLong ullpow10(uLong x) {
+	uLong ans = 1ull;
 	while (x > 0ull)
 		x--, ans *= 10ull;
 	return ans;
 }
 
-static long double R(ullong i, ullong j) {
+static long double R(uLong i, uLong j) {
 	return ((long double) i) + ((long double) reverse(j)) / ((long double) ullpow10( len(j) ));
 }
 
-static long double getReal(ullong index) {
+static long double getReal(uLong index) {
 	_Bool negative = 0;
 	index % 2ull && (negative = 1, index--);
-	ullong *t = generateIndices(index /= 2ull);
+	uLong *t = generateIndices(index /= 2ull);
 	return (long double) ((negative ? -1 : 1) * R(*t, t[1]));
 }
 
-long double *generateRealsSet(ullong maxIndex) {
+long double *generateRealsSet(uLong maxIndex) {
 	long double *set = (long double *) malloc(maxIndex * sizeof(long double));
-	for (ullong i = maxIndex; i --> 0 ;)
+	for (uLong i = maxIndex; i --> 0 ;)
 		set[i] = getReal(i);
 	return set;
 }
 
-void printReals(ullong maxIndex) {
+void printReals(uLong maxIndex) {
 	if (maxIndex == 0ull)
 		// theoretically 2^64 - 1 ...
 		// in practice, basically forever. or like an hour, idk.
 		maxIndex--;
 	printf("[");
-	for (ullong i = 0ull; i < maxIndex ;) {
+	for (uLong i = 0ull; i < maxIndex ;) {
 		printf("%Lf", getReal(i));
 		if (++i < maxIndex)
 			printf("%s", DELIMITER);
@@ -109,7 +108,7 @@ int main(int argc, char **argv) {
 	if (argc > 2) return printf("Too many arguments provided. Only provide 1 argument for the max index.");
 	if (argc < 2) return printf("Not enough arguments provided. Please provide 1 argument for the max index.");
 
-	ullong maxIndex = strtoull(argv[1], (void *) 0, 10);
+	uLong maxIndex = strtoull(argv[1], (void *) 0, 10);
 
 	printReals(maxIndex);
 	
