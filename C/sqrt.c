51f3135f5e147c09d09b8010f4bd255ce697fac8
Daniel Janusch <djanusch2007@gmail.com>
2023-07-30 21:43:12 -0700

    change sqrt.c to return sqrt(x) instead of sqrt(1/x)

--- C/sqrt.c
+++ C/sqrt.c
@@ -1,30 +1,36 @@
 double atof(const char *_String);
+
+#define printf __builtin_printf
 #define DEBUG_MODE ((_Bool) +0u)
 
 double sqrt(double x) {
-	// √(1/x), 1 / √x
+	// return √(x)
 	if (x < 0) return -1.0f;
 	if (x == 0.0f) return 0.0f;
 
-	double prev1 = x, prev2 = x, start = x;
+	double
+		prev1 = x,
+		prev2 = x,
+		start = x;
 
 	do prev2 = prev1
 		, prev1 = x
 		, x = (prev1 + 1.0f) / (start * prev1 + 1.0f)
 	#if DEBUG_MODE
-		, __builtin_printf(" %.17llf\n", prev1)
+		, printf(" %.17llf\n", prev1)
 	#endif
 		;
 	while (prev1 != x && x != prev2);
-	return x;
+
+	return 1 / x;
 }
 
-int main(int argc, const char **argv) {
+int main(int argc, const char *argv[]) {
 	#if DEBUG_MODE
-	if (argc < 2) return __builtin_printf("No argument provided"), 1;
+	if (argc < 2) return printf("No argument provided"), 1;
 	#endif
-	for (int i = 1; i < argc ;) __builtin_printf(
-		"%.18llf\n",
+	for (int i = 1; i < argc ;) printf(
+		"%.18Lf\n",
 		sqrt( atof(argv[i++]) )
 	);
 	return 0;
