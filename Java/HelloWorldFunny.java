26a51ce27a60517dee89dd1d433a250d65f99c37
Daniel Janusch <djanusch2007@gmail.com>
2023-09-06 20:44:36 -0700

    update HelloWorldFunny class to be more in line with Java practices
    you can't forget the interface and the abstract class.
    Oh, and NEVER leave out the manager or the manager factory.
    Java is such a disgusting language; I hate it so much.
    It is literally the bane of my existence.
    Also, no ar because if AP Computer Science A is still using Java 8, then obviously so should I.
    It doesn't matter that it is like a decade old, it is a perfect language in a perfect state.

--- Java/HelloWorldFunny.java
+++ Java/HelloWorldFunny.java
@@ -1,51 +1,123 @@
+// This is my honest attempt at writing Hello World in Java :)
+
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class HelloWorldFunny extends Object {
 	public static void main(String[] args) {
-		PrintHelloWrapper helloPrinter = PrintHelloWrapperFactory.generateNewHelloPrinter(false);
-		helloPrinter.printHello();
+		ConcreteHelloPrinterWrapperManagerFactory printerManagerFactory = new ConcreteHelloPrinterWrapperManagerFactory();
+		ConcreteHelloPrinterWrapperManager printerManager = printerManagerFactory.nextManager();
+
+		printerManager.addNextPrinter(false);
+
+		ConcreteHelloPrinterWrapper printer = printerManager.getLatestPrinter();
+
+		printer.printHello();
 	}
 }
 
-class PrintHelloWrapper extends Object {
-	protected final boolean printing;
-	protected final PrintStream outputFile;
+interface HelloPrinterInterface {
+	void printHello();
+	boolean getIsPrinting();
+	PrintStream getOutputFile();
+	void setIsPrinting(boolean newIsPrinting);
+	void getOutputFile(PrintStream newOutputFile);
+}
 
-	protected static <T> void printlnToOutputFile(T x) {
-		PrintHelloWrapper.printToOutputFile(x);
-		PrintHelloWrapper.printToOutputFile("\n");
-	}
-	protected static <T> void printToOutputFile(T x) {
-		java.lang.System.out.print(x);
-	}
-	public PrintHelloWrapper(PrintStream _outputFile, boolean _silent) {
-		printing = !_silent;
-		outputFile = _outputFile;
-	}
-	public void printHello() {
-		if (isPrinting() == true) {
-			PrintHelloWrapper.printlnToOutputFile("Hello World");
-		}
-	}
-	public boolean isPrinting() {
+abstract class AbstractHelloPrinterWrapper extends Object {
+	protected boolean printing;
+	protected PrintStream outputFile;
+
+	abstract public void printHello();
+
+	public boolean getIsPrinting() {
 		return printing;
 	}
+
 	public PrintStream getOutputFile() {
 		return outputFile;
 	}
+
+	public abstract void setIsPrinting(boolean newIsPrinting);
+	public abstract void getOutputFile(PrintStream newOutputFile);
 }
 
-class PrintHelloWrapperFactory extends Object {
-	public static PrintHelloWrapper generateNewHelloPrinter(boolean silent) {
-		return new PrintHelloWrapper(java.lang.System.out, silent);
+class ConcreteHelloPrinterWrapper extends AbstractHelloPrinterWrapper implements HelloPrinterInterface {
+	protected static <T> void printlnToOutputFile(T x) {
+		ConcreteHelloPrinterWrapper.printToOutputFile(x);
+		ConcreteHelloPrinterWrapper.printToOutputFile("\n");
+	}
+
+	protected static void printlnToOutputFile() {
+		ConcreteHelloPrinterWrapper.printToOutputFile("\n");
+	}
+
+	protected static <T> void printToOutputFile(T x) {
+		java.lang.System.out.print(x);
+	}
+
+	public ConcreteHelloPrinterWrapper(PrintStream outputFile_, boolean silent_) {
+		this.printing = !silent_;
+		this.outputFile = outputFile_;
+	}
+
+	@Override
+	public void printHello() {
+		if (this.getIsPrinting() == true) {
+			ConcreteHelloPrinterWrapper.printlnToOutputFile("Hello World");
+		}
+	}
+
+	// public boolean getIsPrinting
+	// public PrintStream getOutputFile
+
+	@Override
+	public void setIsPrinting(boolean newIsPrinting) {
+		this.printing = newIsPrinting;
+	}
+
+	@Override
+	public void getOutputFile(PrintStream newOutputFile) {
+		this.outputFile = newOutputFile;
 	}
 }
 
-// generates a shorter bytecode than this (by 13 bits):
-/*
-class HelloWorld1 {
-	public static void main(String argv[]) {
-		System.out.println("Hello World");
+class ConcreteHelloPrinterWrapperManager extends Object {
+	private ArrayList<ConcreteHelloPrinterWrapper> helloPrinters = new ArrayList<ConcreteHelloPrinterWrapper>();
+
+	public ConcreteHelloPrinterWrapper nextPrinter(Boolean silent) {
+		return new ConcreteHelloPrinterWrapper( java.lang.System.out, silent );
+	}
+
+	public void addPrinter(ConcreteHelloPrinterWrapper printer) {
+		this.helloPrinters.add( printer );
+	}
+
+	public ConcreteHelloPrinterWrapper addNextPrinter(Boolean silent) {
+		ConcreteHelloPrinterWrapper nextPrinter = this.nextPrinter( silent );
+		this.helloPrinters.add( nextPrinter );
+		return nextPrinter;
+	}
+
+	public ConcreteHelloPrinterWrapper getPrinter(Integer index) {
+		return this.helloPrinters.get( index );
+	}
+
+	public ConcreteHelloPrinterWrapper getLatestPrinter() {
+		return this.helloPrinters.get( this.helloPrinters.size() - 1 );
+	}
+
+	public Integer numberOfPrintersManaged() {
+		return this.helloPrinters.size();
 	}
 }
-*/
+
+class ConcreteHelloPrinterWrapperManagerFactory extends Object {
+	private ConcreteHelloPrinterWrapperManager _nextManager() {
+		// this code might change in the future
+		return new ConcreteHelloPrinterWrapperManager();
+	}
+	public ConcreteHelloPrinterWrapperManager nextManager() {
+		return this._nextManager();
+	}
+}
