213cfa59b4a08e8c5402e88e8adaa71bd0d8af78
Daniel Janusch <djanusch2007@gmail.com>
2023-10-04 21:12:43 -0700

    update continuum.js to version 1.4

--- JavaScript/continuum.js
+++ JavaScript/continuum.js
@@ -1,28 +1,36 @@
 #!/usr/bin/env js
-// continuum.js v1.3 (c) | Copyright 2023 Daniel E. Janusch
+// continuum.js v1.4 (c) | Copyright 2023 Daniel E. Janusch
 
 // This file is licensed by https://raw.githubusercontent.com/drizzt536/files/main/LICENSE
 // and may only be copied IN ITS ENTIRETY under penalty of law.
 
 ////////////////// long form functions //////////////////
 
-var printReals = (function create_printReals() {
+
+var printReals = (function printReals_closure(consoleWidth=140) {
+	// put in your console width for the argument
+
+	// this version should work anywhere
+
 	// all functions assume valid input
+
 	const isBrowser = (globalThis + "").slice(8, -1).toLowerCase() === "window";
-	var write = isBrowser ?
+	var write = isBrowser ? // technically also Deno
 		(function create_write() {
 
 			var str = "";
 			return function write(string = "", flush = false) {
-				// kind of the same effect as process.stdout.write in node
+				// kind of the same effect as `process.stdout.write` gets in node
 
 				str += string;
-				if (flush || str.length > 100)
-					console.log.call(console, str),
+
+				if (flush || str.length > consoleWidth)
+					console.log(str),
 					str = "";
 			}
 
 		})() :
+		// just `process.stdout.write` doesn't work here:
 		str => process.stdout.write.call(process.stdout, str);
 
 	// bit shifts are probably faster than division.
@@ -43,38 +51,41 @@ var printReals = (function create_printReals() {
 		// less than or equal to the input `x`.
 		// I do not remember how I derived this.
 
-		return ( isqrt(1n + (x << 3n)) + 1 >> 1n ) - 1n;
+		return ( isqrt(1n + (x << 3n)) + 1n >> 1n ) - 1n;
 	}
 	function triangleNumber(n) {
 		// `n` is the "triangle index"
 		return n * (n + 1n) >> 1n;
 	}
-	function generateCoordinates(x) {
-		// converts a 1d index into an appropriate 2d coordinate
-		const u = greatestTriangleIndex(x)
-			, k = triangleNumber(u)
-			, t = x - k;
 
-		return [t, u - t];
+	function generateCoordinates(n) {
+		// converts a 1d index into an appropriate 2d coordinate
+		const k = greatestTriangleIndex(n)
+			, t = triangleNumber(k)
+			, u = n - t;
+
+		return [u, k - u];
 		// these can be swapped, but it changes inverse()
 	}
-	function R(i, j) {
-		// `i`: integer part
-		// `j`: reverse of decimal part
-		return `${i}.` + `${j}`.split("").reverse().join("");
+	function R(n, k) {
+		// `n`: integer part
+		// `k`: reversed decimal part
+		return `${n}.` + `${k}`.split("").reverse().join("");
 	}
 	function getReal(index) {
 		// generates a unique real number based on the index given.
-
+		// sign == (n+1) ~Mod~ 2 - 1
+		// sign == Mod[n + 1, 2] - 1
 		return (index % 2n ? "-" : "") + // these two cases can be swapped, but it changes inverse()
 			R( ...generateCoordinates(index >> 1n) );
 	}
 	function inverse(string) {
-		const match = /^-??(\d+)\.(\d+)$/.exec(string);
-		const i = BigInt( match[1] );
-		const j = BigInt( match[2].split("").reverse().join("") );
+		const
+			{groups: {ipart, fpart}} = /^-??(?<ipart>\d+)\.(?<fpart>\d+)$/.exec(string),
+			n = BigInt( ipart ),
+			k = BigInt( fpart.split("").reverse().join("") );
 
-		return (i+j)**2n + 3n*i + j + BigInt(string[0] === "-")
+		return (n+k)**2n + 3n*n + k + BigInt(string[0] === "-")
 	}
 
 	function generateRealsList(maxIndex=10n) {
@@ -123,49 +134,54 @@ function isqrt(n) {
 	return x0;
 }
 function indexToReal(n = 1n) {
-	const a = isqrt(1n + (n << 2n)) + 1n >> 1n; // usually n << 3n, but this skips a step
-	const b = n - a*(a - 1n) >> 1n; // integer part = n/2 - T(a)
+	const
+		a = isqrt(1n + (n << 2n)) + 1n >> 1n, // usually n << 3n, but this skips a step
+		b = n - a*(a - 1n) >> 1n; // integer part = n/2 - T(a)
 
 	return `${n % 2n ? "-" : ""}${b}.` + `${a - b - 1n}`.split("").reverse().join("");
 }
 function realToIndex(string) {
-	const match = /^-??(\d+)\.(\d+)$/.exec(string);
-	const x = BigInt(match[1]);
-	const y = BigInt(match[2].split("").reverse().join(""));
+	const
+		{groups: {ipart, fpart}} = /^-??(?<ipart>\d+)\.(?<fpart>\d+)$/.exec(string),
+		x = BigInt( ipart ),
+		y = BigInt( fpart.split("").reverse().join("") );
 
-	return (x+y)**2n + 3n*x + y + BigInt(string[0] === "-");
+	return (x+y)**2n + 3n*x + y + BigInt(string[0] === "-")
 }
-
 
 ////////////////// short form versions using lib.js //////////////////
 
 function indexToReal(n = 1n) {
-	const a = bMath.sqrt(1n + (n << 2n)) + 1n >> 1n; // usually n << 3n, but this skips a step
-	const b = n - a*(a - 1n) >> 1n; // integer part = n/2 - T(a)
+	const
+		a = bMath.sqrt(1n + (n << 2n)) + 1n >> 1n, // usually n << 3n, but this skips a step
+		b = n - a*(a - 1n) >> 1n; // integer part = n/2 - T(a)
 
 	return `${n % 2n ? "-" : ""}${b}.` + `${a - b - 1n}`.reverse();
 }
 function realToIndex(string) {
-	const match = /^-??(\d+)\.(\d+)$/.exec(string);
-	const x = BigInt(match[1]);
-	const y = BigInt(match[2].reverse());
+	const
+		{groups: {ipart, fpart}} = /^-??(?<ipart>\d+)\.(?<fpart>\d+)$/.exec(string),
+		x = BigInt( ipart ),
+		y = BigInt( fpart.reverse() );
 
-	return (x+y)**2n + 3n*x + y + BigInt(string[0] === "-");
+	return (x+y)**2n + 3n*x + y + BigInt(string[0] === "-")
 }
 
 
 ////////////////// short forms using lib.js and floating-point double-precision numbers //////////////////
 
 function indexToReal(n = 1) {
-	const a = floor( (rMath.isqrt(1 + 4*n) + 1) / 2 ); // usually n << 3n, but this skips a step
-	const b = floor( (n - a*(a - 1)) / 2 ); // integer part = n/2 - T(a)
+	const
+		a = floor( (rMath.isqrt(1 + 4*n) + 1) / 2 ), // usually n << 3n, but this skips a step
+		b = floor( (n - a*(a - 1)) / 2 ); // integer part = n/2 - T(a)
 
 	return `${n % 2 ? "-" : ""}${b}.` + `${a - b - 1}`.reverse();
 }
 function realToIndex(string) {
-	const match = /^-??(\d+)\.(\d+)$/.exec(string);
-	const x = +match[1];
-	const y = +match[2].reverse();
+	const
+		{groups: {ipart, fpart}} = /^-??(?<ipart>\d+)\.(?<fpart>\d+)$/.exec(string),
+		x = +ipart,
+		y = +fpart.reverse();
 
-	return (x+y)**2 + 3*x + y + (string[0] === "-");
+	return (x+y)**2 + 3*x + y + (string[0] === "-")
 }
