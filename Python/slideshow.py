eba99fc47f449f69c3dbe67185c60a1ce89814ba
Daniel Janusch <djanusch2007@gmail.com>
2024-04-01 17:34:09 -0700

    update slideshow.py

--- Python/slideshow.py
+++ Python/slideshow.py
@@ -8,29 +8,41 @@ Also kind of like google slides, but only with images, and different.
 # TODO: add more right-hand control options, ie: with arrow keys, mouse, etc.
 # TODO: allow keybind control
 # TODO: add controls to swap image ordering
+# TODO: add function to unhide most recently hidden path
 # TODO: make the beginning function not actually traverse the stack
 # TODO: add shift-space : unpause but don't go to the next one, just start the timer
+# TODO: add `--silent` or `--quiet` argument, or `--verbose`
+# TODO: option to change timing without restart
+# TODO: add keybind for `enlarge to fit closest edge
+# TODO: use `collections.deque` for the stacks
+# TODO: make numpad 8, 4, 5, 6 be the same as wasd.
+# TODO: make a `go to the end` function. go until the next stack is empty
+# TODO: make w/NP_8 and s/NP_5 have some function
 # TODO/Maybe: add extra statistics to details?
-# TODO/Maybe: allow multi-monitor support
+# TODO/Maybe: allow multi-monitor support?
+# TODO/Maybe: allow the user to choose the background color?
 
 from os        import path
 from sys       import exit
 from PIL       import Image, ImageTk # Pillow
+from random    import randrange
 from tkinter   import Canvas, Tk
 from argparse  import ArgumentParser
 from functools import partial
 
 #### argument parsing
 
-arg_parser = ArgumentParser(add_help = False, description = "image slideshow program")
+arg_parser = ArgumentParser(add_help=False, description="image slideshow program")
 
 arg_parser.add_argument("--change-time", "-c", type=int, default=5000, help="time between swaps (in ms). defaults to 5 seconds")
 arg_parser.add_argument("--stack-size", "-s", type=int, default=-1, help="stack size for previous images. defaults to no limit (-1)")
 arg_parser.add_argument("--paused", "-p", action="store_true", default=False, help="start paused. defaults to false")
 arg_parser.add_argument("--help", "-h", action="store_true", default=False, help="print commands and this usage message and exit")
 arg_parser.add_argument("--sequential", "-S", action="store_true", default=False, help="load images sequentially instead of randomly. defaults to false")
-arg_parser.add_argument("--include-all", "-i", action="store_true", default=False, help="This switch turns off the behavior described here. ignore files with the following extensions: .txt, .text, .md, and .org. Ignore folders called ignore. Ignore files called readme, license, or ignore.")
+arg_parser.add_argument("--include-all", "-i", action="store_true", default=False, help="This switch turns off the behavior described here. Ignore files with the following extensions: .txt, .text, .md, and .org. Ignore folders called ignore. Ignore files called readme, license, or ignore.")
+arg_parser.add_argument("--no-subdirs", "-n", action="store_true", default=False, help="Do not include subdirectories of directories passed in as arguments")
 arg_parser.add_argument("paths", nargs="*", type=str, help="paths to directories or files with images")
+
 
 args = arg_parser.parse_args()
 
@@ -39,25 +51,45 @@ help_arg = args.help
 directories = args.paths
 sequential = args.sequential
 ignore = not args.include_all
+allow_subdirs = not args.no_subdirs
 swap_time = args.change_time
 stack_size = args.stack_size
 
 if not directories and not help_arg:
-	from getpass import getuser
 	from os import name as osname
 
-	if osname != "nt":
-		print("The default directory is only supported on Windows")
+	if osname == "nt":
+		from getpass import getuser
+		# I don't know what the default directory should be on other platforms
+		directories = f"C:/Users/{getuser()}/Pictures/"
+		del getuser
+	elif osname == "posix":
+		directories = "~/Pictures"
+	elif osname == "java":
+		from re import sub
+
+		password = input("Do you like Java?")
+
+		if sub(r"[\?,]", "", password.lower()) in {"no", "n", "0", "no what the fuck", "no fuck you", "who the fuck does", "do i look like a sadist"}:
+			print("No directory given. The default directory is only supported on Windows and POSIX.")
+			exit(1)
+		else:
+			# TODO: find malware and execute it here.
+
+			while True:
+				print("Fuck Java, Fuck Jython, Fuck you")
+	else:
+		# ChatGPT says this is either "java" or "os2". I don't care about either of them
+		print("No directory given. The default directory is only supported on Windows and POSIX")
 		exit(1)
 
-	directories = f"C:/Users/{getuser()}/Pictures/"
 	print(f"No directory given; defaulting to {directories !r}")
 
-	del getuser, osname
+	del osname
 
 #### basic things
 
-def get_images_recursively(root: list[str, ...] = "./") -> tuple[str, ...]:
+def get_images_recursively(root: list[str, ...] = "./") -> list[str, ...]:
 	"recursively gets images from the directory and returns them"
 
 	def ls(directory: str = "./", /) -> list[str, ...]:
@@ -83,8 +115,7 @@ def get_images_recursively(root: list[str, ...] = "./") -> tuple[str, ...]:
 
 		if not path.isdir(directory):
 			if path.isfile(directory):
-				# file was given directly, so
-				# bypass the text file ignore
+				# file was given directl so bypass the text file ignore
 				files.append(directory)
 
 			# the path does not exist
@@ -93,16 +124,17 @@ def get_images_recursively(root: list[str, ...] = "./") -> tuple[str, ...]:
 		if ignore and directory == "ignore":
 			continue
 
+		# subdirectories
 		for name in ls(directory):
 			if path.isdir(name):
-				directories.append(name)
+				if allow_subdirs:
+					directories.append(name)
 			elif ignore and name.lower() not in {"readme", "license", "ignore"} and\
 				extname(name).lower() not in {".txt", ".text", ".md", ".org"}:
 
 				files.append(name)
 
-	# I do not remember what the purpose of this `list()+` was. Create a new list?
-	return list()+[file for file in files if file.lower().endswith((
+	return [file for file in files if file.lower().endswith((
 		# .avif is not supported by Pillow
 		".bmp",
 		".gif",
@@ -140,6 +172,9 @@ if not original_image_paths and not help_arg:
 
 del directories
 
+def current_image_data() -> tuple[Image.Image, str, list[int, int], tuple[int, int]]:
+	return image, image_path, translation, original_size
+
 def resized_image(scale: int | float = None):
 	"""
 		if no scale is given, the image will be resized to
@@ -147,7 +182,7 @@ def resized_image(scale: int | float = None):
 		returns a PIL.ImageTk.PhotoImage instance
 	"""
 
-	global image, image_path, original_size, screen_width, screen_height
+	global original_size
 
 	img = Image.open(image_path)
 	width, height = img.size
@@ -170,7 +205,6 @@ def resized_image(scale: int | float = None):
 
 def next_image_path() -> str:
 	"returns the absolute path of the next image"
-	from random import randrange
 
 	global image_paths
 
@@ -180,18 +214,18 @@ def next_image_path() -> str:
 
 	if not image_paths:
 		print("no images left. reshuffling")
-		image_paths = list(original_image_paths)
+		image_paths = original_image_paths.copy()
 
 	index = len(image_paths) - 1 if sequential else randrange(len(image_paths))
 
 	return image_paths.pop(index)
 
-def display_image(image) -> None:
+def display_image(image: ImageTk.PhotoImage) -> None:
 	"Takes in whatever ImageTk.PhotoImage(img) returns. centers the image on the screen."
 
 	location =\
-		screen_center[0] + translate_x,\
-		screen_center[1] + translate_y
+		screen_center[0] + translation[0],\
+		screen_center[1] + translation[1]
 
 	canvas.delete("all")
 	canvas.create_image(*location, anchor="center", image=image)
@@ -201,6 +235,7 @@ def display_image(image) -> None:
 def print_commands(event=None) -> None:
 	from re import sub as replace, MULTILINE
 
+	# TODO: make this not outdated
 	print(replace(pattern=r"^[\t ]*?\|", flags=MULTILINE, repl="", string="""\
 		|(the commands here use Emacs syntax)
 		|basic commands:
@@ -362,23 +397,13 @@ def print_hidden_paths(event=None) -> None:
 		print(f"\t{path}")
 
 def reset(event=None) -> None:
-	global image_paths,\
-		original_size,\
-		prev_image_stack,\
-		next_image_stack,\
-		translate_x,\
-		translate_y,\
-		image_angle,\
-		image,\
-		image_path,\
-		paused
+	global image_paths, original_size, prev_image_stack, next_image_stack, translation, image_angle, image, image_path, paused
 
-	image_paths = list(original_image_paths)
+	image_paths = original_image_paths.copy()
 	original_size = None
 	prev_image_stack = []
 	next_image_stack = []
-	translate_x = 0
-	translate_y = 0
+	translation = [0, 0]
 	image_angle = 0
 	image = None
 	image_path = None
@@ -387,16 +412,15 @@ def reset(event=None) -> None:
 	next_image()
 
 def reset_changes(event=None) -> None:
-	global translate_x, translate_y, image
+	global translation, image
 
-	translate_x = 0
-	translate_y = 0
+	translation = [0, 0]
 
-	# undo rotations
+	# undo rotation-circularization and zoom-blur
 	img = Image.open(image_path).resize(original_size, resample=Image.LANCZOS)
 	image = ImageTk.PhotoImage(img)
 
-	# redisplay the image with the new translation
+	# redisplay the image with the new translation/rotation/zoom
 	display_image(image)
 
 def print_details(event=None) -> None:
@@ -409,7 +433,7 @@ def print_details(event=None) -> None:
 	print(f"\thidden images: {len(hidden_images)}")
 	print(f"\timages left: {len(image_paths)}")
 	print(f"\tpaused: {paused}")
-	print(f"\ttranslation: ({translate_x}, {translate_y})")
+	print(f"\ttranslation: {tuple(translation)}")
 	print(f"\timage zoom: {image.width() / original_size[0]}")
 
 def next_image(event=None) -> None:
@@ -420,30 +444,21 @@ def next_image(event=None) -> None:
 		# This stops it from unpausing
 		event = True
 
-	# without `global image`, it won't load to the canvas
-	global image_path,\
-		original_size,\
-		translate_x,\
-		translate_y,\
-		image,\
-		timer_id
+	global image_path, translation, original_size, image, timer_id
 
 	if image is not None:
-		prev_image_stack.append(
-			(image, image_path, translate_x, translate_y, original_size)
-		)
+		prev_image_stack.append(current_image_data())
 
 	if stack_size != -1 and len(prev_image_stack) > stack_size:
 		# remove the oldest one
 		prev_image_stack.pop(0)
 
 	if next_image_stack:
-		image, image_path, translate_x, translate_y, original_size = next_image_stack.pop()
+		image, image_path, translation, original_size = next_image_stack.pop()
 	else:
 		image_path = next_image_path()
-		image = resized_image() # uses the `image_path` global
-		translate_x = 0
-		translate_y = 0
+		image = resized_image()
+		translation = [0, 0]
 
 	display_image(image)
 	hide_cursor()
@@ -458,18 +473,12 @@ def next_image(event=None) -> None:
 	timer_id = root.after(swap_time, next_image)
 
 def prev_image(event=None) -> None:
-	global image,\
-		image_path,\
-		translate_x,\
-		translate_y,\
-		original_size
+	global image, image_path, translation, original_size
 
 	if prev_image_stack:
-		next_image_stack.append(
-			(image, image_path, translate_x, translate_y, original_size)
-		)
+		next_image_stack.append(current_image_data())
 
-		image, image_path, translate_x, translate_y, original_size = prev_image_stack.pop()
+		image, image_path, translation, original_size = prev_image_stack.pop()
 		display_image(image)
 	else:
 		print("image stack underflow. retaining the current image")
@@ -477,12 +486,11 @@ def prev_image(event=None) -> None:
 def pop_image(event=None) -> None:
 	"goes to the next image and removes the current one from the stack"
 
-	global image, image_path, translate_x, translate_y
+	global image, image_path, translation
 
 	image = None
 	image_path = None
-	translate_x = 0
-	translate_y = 0
+	translation = [0, 0]
 
 	next_image()
 
@@ -491,7 +499,11 @@ def hide_image(event=None) -> None:
 
 	hidden_images.append(image_path)
 	original_image_paths.remove(image_path)
+
 	# TODO: look through prev_image_stack and next_image_stack for this image.
+	# for i in range(len(prev_image_stack)):
+	# 	;
+
 	pop_image()
 
 def delete_image(event=None) -> None:
@@ -511,7 +523,11 @@ def delete_image(event=None) -> None:
 
 	hide_image(image)
 
-def prev_image_n(event = None, /, *, n: int = 1) -> None:
+def next_image_n(event=None, /, *, n: int = 1) -> None:
+	for i in range(n):
+		next_image(event)
+
+def prev_image_n(event=None, /, *, n: int = 1) -> None:
 	for i in range(n):
 		prev_image(event)
 
@@ -540,16 +556,11 @@ def close_canvas(event=None) -> None:
 	root.destroy()
 	exit(0)
 
-def translate(event=None, /, *, dir: str = "x", diff: int = 1) -> None:
-	if dir != "x" and dir != "y":
-		raise ValueError("direction must either by x or y")
+def translate(event=None, vec: tuple[int, int] = (0, 0)) -> None:
+	"translate the image by an x and y amount of pixels, and redisplay the image."
 
-	if dir == "x":
-		global translate_x
-		translate_x += diff
-	else:
-		global translate_y
-		translate_y += diff
+	translation[0] += vec[0]
+	translation[1] += vec[1]
 
 	display_image(image)
 
@@ -560,7 +571,6 @@ def hide_cursor(event=None) -> None:
 	root.config(cursor="none")
 
 def beginning(event=None) -> None:
-	global next_image_stack, prev_image_stack
 	print("beginning")
 
 	while prev_image_stack:
@@ -587,14 +597,21 @@ def zoom(event = None, /, *, scale: float = 1.5) -> None:
 
 def rotate(event = None, /, *, angle: float = 90) -> None:
 	"rotate the displayed image"
+	# TODO: add skew rotation for non 90n angles.
 
-	global image, image_angle
-
-	img = ImageTk.getimage(image)
+	global image #, image_angle
 
 	angle %= 360
 
+	if angle == 0:
+		return
+
+	img = ImageTk.getimage(image)
+
+
 	# these transpositions are much faster than img.rotate()
+	# rotate the large amount first
+	# should be faster and be more consistent with the original image
 	if angle >= 270:
 		img = img.transpose(Image.ROTATE_270)
 		angle -= 270
@@ -625,74 +642,86 @@ canvas.pack(expand=True, fill="both")
 
 
 # basic commands
-root.bind("<Control-KeyPress-C>", close_canvas)
-root.bind("<KeyPress-f>", print_details)
-root.bind("<KeyPress-F>", print_hidden_paths)
-root.bind("<Control-KeyPress-R>", reset)
-root.bind("<KeyPress-r>", reset_changes)
-root.bind("<KeyPress-/>", print_commands)
-root.bind("<KeyPress-h>", hide_image)
-root.bind("<KeyPress-b>", beginning)
-root.bind("<KeyPress-d>", next_image)
-root.bind("<KeyPress-X>", pop_image)
-root.bind("<KeyPress-a>", prev_image)
+root.bind("<Control-C>", close_canvas)
+root.bind("<f>", print_details)
+root.bind("<F>", print_hidden_paths)
+root.bind("<Control-R>", reset)
+root.bind("<r>", reset_changes)
+root.bind("</>", print_commands)
+root.bind("<h>", hide_image)
+root.bind("<b>", beginning)
+root.bind("<d>", next_image)
+root.bind("<X>", pop_image)
+root.bind("<a>", prev_image)
 root.bind("<space>", pauseplay)
-root.bind("<Control-Alt-Shift-KeyPress-BackSpace>", delete_image)
+root.bind("<Control-Alt-Shift-BackSpace>", delete_image)
 
 
-# number keys
-root.bind("<KeyPress-1>", partial(prev_image_n, n=1))
-root.bind("<KeyPress-2>", partial(prev_image_n, n=2))
-root.bind("<KeyPress-3>", partial(prev_image_n, n=3))
-root.bind("<KeyPress-4>", partial(prev_image_n, n=4))
-root.bind("<KeyPress-5>", partial(prev_image_n, n=5))
-root.bind("<KeyPress-6>", partial(prev_image_n, n=6))
-root.bind("<KeyPress-7>", partial(prev_image_n, n=7))
-root.bind("<KeyPress-8>", partial(prev_image_n, n=8))
-root.bind("<KeyPress-9>", partial(prev_image_n, n=9))
+# number keys: go backward
+root.bind("<1>", partial(prev_image_n, n=1))
+root.bind("<2>", partial(prev_image_n, n=2))
+root.bind("<3>", partial(prev_image_n, n=3))
+root.bind("<4>", partial(prev_image_n, n=4))
+root.bind("<5>", partial(prev_image_n, n=5))
+root.bind("<6>", partial(prev_image_n, n=6))
+root.bind("<7>", partial(prev_image_n, n=7))
+root.bind("<8>", partial(prev_image_n, n=8))
+root.bind("<9>", partial(prev_image_n, n=9))
+
+# shift number keys: go foreward
+root.bind("<Shift-1>", partial(next_image_n, n=1))
+root.bind("<Shift-2>", partial(next_image_n, n=2))
+root.bind("<Shift-3>", partial(next_image_n, n=3))
+root.bind("<Shift-4>", partial(next_image_n, n=4))
+root.bind("<Shift-5>", partial(next_image_n, n=5))
+root.bind("<Shift-6>", partial(next_image_n, n=6))
+root.bind("<Shift-7>", partial(next_image_n, n=7))
+root.bind("<Shift-8>", partial(next_image_n, n=8))
+root.bind("<Shift-9>", partial(next_image_n, n=9))
 
 
 # zoom
-root.bind("<KeyPress-z>", partial(zoom, scale=4/5)) # zoom out
-root.bind("<KeyPress-c>", partial(zoom, scale=5/4)) # zoom in
+root.bind("<z>", partial(zoom, scale=4/5)) # zoom out
+root.bind("<c>", partial(zoom, scale=5/4)) # zoom in
 
-root.bind("<KeyPress-Z>", partial(zoom, scale=0.99)) # zoom out
-root.bind("<KeyPress-C>", partial(zoom, scale=1 / 0.99)) # zoom in
+root.bind("<Z>", partial(zoom, scale=0.99)) # zoom out
+root.bind("<C>", partial(zoom, scale=1 / 0.99)) # zoom in
 
 
 # translation
-fn = partial(translate, dir="y", diff=-10)
-root.bind("<KeyPress-Up>", fn)
-root.bind("<KeyPress-W>", fn)
+fn = partial(translate, vec=(0, -10))
+root.bind("<Up>", fn)
+root.bind("<W>", fn)
 
-fn = partial(translate, dir="x", diff=10)
-root.bind("<KeyPress-Right>", fn)
-root.bind("<KeyPress-D>", fn)
+fn = partial(translate, vec=(10, 0))
+root.bind("<Right>", fn)
+root.bind("<D>", fn)
 
-fn = partial(translate, dir="y", diff=10)
-root.bind("<KeyPress-Down>", fn)
-root.bind("<KeyPress-S>", fn)
+fn = partial(translate, vec=(0, 10))
+root.bind("<Down>", fn)
+root.bind("<S>", fn)
 
-fn = partial(translate, dir="x", diff=-10)
-root.bind("<KeyPress-Left>", fn)
-root.bind("<KeyPress-A>", fn)
+fn = partial(translate, vec=(-10, 0))
+root.bind("<Left>", fn)
+root.bind("<A>", fn)
 
 
-fn = partial(translate, dir="y", diff=-1)
-root.bind("<Shift-KeyPress-Up>", fn)
-root.bind("<Control-KeyPress-w>", fn)
+fn = partial(translate, vec=(0, -1))
+root.bind("<Shift-Up>", fn)
+root.bind("<Control-w>", fn)
 
-fn = partial(translate, dir="x", diff=1)
-root.bind("<Shift-KeyPress-Right>", fn)
-root.bind("<Control-KeyPress-d>", fn)
+fn = partial(translate, vec=(1, 0))
+root.bind("<Shift-Right>", fn)
+root.bind("<Control-d>", fn)
 
-fn = partial(translate, dir="y", diff=1)
-root.bind("<Shift-KeyPress-Down>", fn)
-root.bind("<Control-KeyPress-s>", fn)
+fn = partial(translate, vec=(0, 1))
+root.bind("<Shift-Down>", fn)
+root.bind("<Control-s>", fn)
 
-fn = partial(translate, dir="x", diff=-1)
-root.bind("<Shift-KeyPress-Left>", fn)
-root.bind("<Control-KeyPress-a>", fn)
+fn = partial(translate, vec=(-1, 0))
+root.bind("<Shift-Left>", fn)
+root.bind("<Control-a>", fn)
+
 del fn
 
 
