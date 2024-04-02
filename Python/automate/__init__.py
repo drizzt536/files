5f5ee31e7e1208c1461ba0b495ada5d63a0c2f34
Daniel Janusch <djanusch2007@gmail.com>
2024-04-01 17:32:19 -0700

    update automate Python library

--- Python/automate/__init__.py
+++ Python/automate/__init__.py
@@ -13,9 +13,12 @@ from pynput.mouse import (
 	Events as MouseEvents
 )
 """
-pynput: https://pypi.org/project/pynput/
+uses pynput: https://pypi.org/project/pynput/
 file location:
 C:/Python[version]/Lib/site-packages/automate/__init__.py
+
+basically provides a functional/procedural? wrapper around pynput.
+pynput uses classes and that is icky
 """
 
 
@@ -26,53 +29,59 @@ buttons = [] # (mouse) buttons currently pressed
 keys = [] # keys currently pressed
 
 __variables__ = (
-	'buttons',
-	'keys',
-	'Button',
-	'Key',
-	'__variables__',
-	'__functions__',
-	'__all__',
-	'fkeys',
-	'winkey (Key.cmd)',
+	"buttons",
+	"keys",
+	"Button",
+	"Key",
+	"__variables__",
+	"__functions__",
+	"__all__",
+	"fkeys",
+	"winkey (Key.cmd)",
 	"misc (module)",
 )
 __functions__ = (
-	'KeyboardController',
-	'KeyboardListener',
-	'KeyboardEvents',
-	'HotKey',
-	'GlobalHotKeys',
-	'MouseController',
-	'MouseListener',
-	'MouseEvents',
-	'getbuttonspressed',
-	'getkeyspressed',
-	'getmouseposition',
-	'setmouseposition',
-	'movemouse',
-	'click',
-	'scroll',
-	'vscroll',
-	'hscroll',
-	'mousedown',
-	'mouseup',
-	'releasebuttons',
-	'keydown',
-	'keyup',
-	'keypress',
-	'typekeys',
-	'keycombo',
-	'nextwindow',
-	'prevwindow',
-	'releasekeys',
-	'release_all',
-	'volumeup',
-	'volumedown',
-	'pauseplay',
-	'togglemute',
-	'nextmedia',
-	'prevmedia'
+	"KeyboardController",
+	"KeyboardListener",
+	"KeyboardEvents",
+	"HotKey",
+	"GlobalHotKeys",
+	"MouseController",
+	"MouseListener",
+	"MouseEvents",
+	"getbuttonspressed",
+	"getkeyspressed",
+	"getmouseposition",
+	"setmouseposition",
+	"setmouseposition2",
+	"movemouse",
+	"click",
+	"drag",
+	"leftclick",
+	"rightclick",
+	"middleclick",
+	"scroll",
+	"vscroll",
+	"hscroll",
+	"mousedown",
+	"mouseup",
+	"releasebuttons",
+	"keydown",
+	"keyup",
+	"keypress",
+	"typekeys",
+	"typestring",
+	"keycombo",
+	"nextwindow",
+	"prevwindow",
+	"releasekeys",
+	"release_all",
+	"volumeup",
+	"volumedown",
+	"pauseplay",
+	"togglemute",
+	"nextmedia",
+	"prevmedia"
 )
 __all__ = __variables__ + __functions__
 fkeys = (
@@ -102,140 +111,299 @@ Key.u = "u"; Key.U = "U"; Key.v = "v"; Key.V = "V"
 Key.w = "w"; Key.W = "W"; Key.x = "x"; Key.X = "X"
 Key.y = "y"; Key.Y = "Y"; Key.z = "z"; Key.Z = "Z"
 Key.shft = Key.shift
+Key.control = Key.ctrl
+
+def init_module() -> None:
+	from win32api import Sleep
+
+	mouse_controller = MouseController()
+	keyboard_controller = KeyboardController()
+	realnumber = float | int
+	Location = tuple[int, int]
+
+	global getbuttonspressed,\
+		getkeyspressed,\
+		getmouseposition,\
+		setmouseposition,\
+		setmouseposition2,\
+		movemouse,\
+		click,\
+		drag,\
+		leftclick,\
+		rightclick,\
+		middleclick,\
+		scroll,\
+		vscroll,\
+		hscroll,\
+		mousedown,\
+		mouseup,\
+		releasebuttons,\
+		keydown,\
+		keyup,\
+		keypress,\
+		typekeys,\
+		enter,\
+		keycombo,\
+		nextwindow,\
+		prevwindow,\
+		releasekeys,\
+		releaseall,\
+		volumeup,\
+		volumedown,\
+		pauseplay,\
+		togglemute,\
+		nextmedia,\
+		prevmedia,\
+		Sleep
+
+
+	# getters
+
+	def getbuttonspressed() -> list[Key, ...]:
+		return buttons
+
+	def getkeyspressed() -> list[Key, ...]:
+		return keys
+
+	def getmouseposition() -> tuple[int, int]:
+		return mouse_controller.position
 
 
 
-# getters
+	# Mouse automation
 
-def getbuttonspressed() -> list[Key, ...]:
-	return buttons
+	def setmouseposition(x: int | Location = 0, y: int = 0) -> None:
+		mouse_controller.position = x if isinstance(x, tuple) else (x, y)
 
-def getkeyspressed() -> list[Key, ...]:
-	return keys
+	def setmouseposition2(loc: Location = None, steps: int = 50, delay_ms: int = 2) -> None:
+		"change mouse position smoothly. takes `steps` number of increments"
 
-def getmouseposition() -> tuple[int, int]:
-	return MouseController().position
+		rel_x = loc[0] - mouse_controller.position[0]
+		rel_y = loc[1] - mouse_controller.position[1]
 
+		movemouse2((rel_x, rel_y), steps, delay_ms)
 
+	def movemouse(rel_x: int | Location = 0, rel_y: int = 0) -> None:
+		if isinstance(rel_x, tuple):
+			mouse_controller.move(*rel_x)
+		else:
+			mouse_controller.move(rel_x, rel_y)
 
-# Mouse automation
+	def movemouse2(rel_loc: Location, steps: int = 50, delay_ms: int = 2) -> None:
+		"move mouse smoothly. takes `steps` number of increments. sleep `delay_ms` between each move."
+		rel_x, rel_y = rel_loc
 
-def setmouseposition(x: float | int = 0, y: float | int = 0) -> None:
-	MouseController().position = (x, y)
+		x_step = rel_x // steps
+		y_step = rel_y // steps
 
-def movemouse(relX: float | int = 0, relY: float | int = 0) -> None:
-	MouseController().move(relX, relY)
+		for i in range(steps):
+			mouse_controller.move(x_step, y_step)
+			Sleep(delay_ms)
 
-def click(button: object = Button.left, times: int = 1) -> None:
-	MouseController().click(button, times)
+		# make sure it moves to the exact target given
+		mouse_controller.move(rel_x % steps, rel_y % steps)
 
-def scroll(ltr: float | int = 0, dtu: float | int = 0) -> None:
-	MouseController().scroll(rtl, dtu)
+	def click(button: object = Button.left, times: int = 1, loc: Location | None = None) -> None:
+		if loc is not None:
+			setmouseposition(loc)
 
-def vscroll(dtu: float | int = 0) -> None: # vertical scroll
-	MouseController().scroll(0, dtu)
+		mouse_controller.click(button, times)
 
-def hscroll(ltr: float | int = 0) -> None: # horizontal scroll
-	MouseController().scroll(ltr, 0)
+	def drag(
+		start: Location | None = None,
+		end: Location | None = None,
+		button: Button = Button.left,
+		move_steps: int = 2,
+		delay_ms: int = 10,
+	) -> None:
 
-def mousedown(button: object = Button.left) -> None:
-	buttons.append(button)
-	MouseController().press(button)
+		"""
+		drag the mouse button `button` between locations `start` and `end`
+		If `start` isn't provided, it defaults to the current position.
+		`end` must be provided, or else it is just a click.
+		`move_steps` is the number of steps it takes to move the mouse between points,
+		set higher for smoother movement.
+		sleeps `delay_ms` between mousedown & movement, between steps, and between movement & mouseup
+		"""
 
-def mouseup() -> None: # release most recent mouse button
-	MouseController().release( buttons.pop() )
+		if end is None:
+			raise ValueError("the end location must be provided.")
 
-def releasebuttons() -> None:
-	while buttons:
+		mousedown(button, loc=start, delay_ms=10)
+
+		Sleep(delay_ms)
+
+		setmouseposition(end) if move_steps == 1 else setmouseposition2(end, move_steps, delay_ms)
+
+		Sleep(delay_ms)
+
 		mouseup()
 
+	def leftclick(times: int = 1, loc: Location | None = None) -> None:
+		if loc is not None:
+			mouse_controller.position = loc
+
+		mouse_controller.click(Button.left, times)
+
+	def rightclick(times: int = 1, loc: Location | None = None) -> None:
+		if loc is not None:
+			mouse_controller.position = loc
+
+		mouse_controller.click(Button.right, times)
+
+	def middleclick(times: int = 1, loc: Location | None = None) -> None:
+		if loc is not None:
+			mouse_controller.position = loc
+
+		mouse_controller.click(Button.middle, times)
+
+	def scroll(ltr: realnumber | Location = 0, dtu: realnumber = 0) -> None:
+		if isinstance(ltr, tuple):
+			mouse_controller.scroll(*ltr)
+		else:
+			mouse_controller.scroll(ltr, dtu)
+
+	def vscroll(dtu: realnumber = 0) -> None:
+		"vertical scroll"
+		mouse_controller.scroll(0, dtu)
+
+	def hscroll(ltr: realnumber = 0) -> None:
+		"horizontal scroll"
+		mouse_controller.scroll(ltr, 0)
+
+	def mousedown(button: Button = Button.left, loc: Location | None = None, delay_ms: int = 10) -> None:
+		"the delay only happens if the mouse location is changed"
+		if loc is not None:
+			mouse_controller.position = loc
+
+			Sleep(delay_ms)
+
+		buttons.append(button)
+		mouse_controller.press(button)
+
+	def mouseup(loc: Location | None = None, delay_ms: int = 10) -> None:
+		"release most recent mouse button"
+
+		if loc is not None:
+			mouse_controller.position = loc
+
+			Sleep(delay_ms)
+
+		mouse_controller.release( buttons.pop() )
+
+	def releasebuttons() -> None:
+		while buttons:
+			mouseup()
 
 
-# Keyboard automation
 
-def keydown(key: object = None) -> None:
-	if key is None: return
-	keys.append(key)
-	KeyboardController().press(key)
+	# Keyboard automation
 
-def keyup() -> None: # release most recent key
-	KeyboardController().release( keys.pop() )
+	def keydown(key: object = None) -> None:
+		if key is None:
+			return
 
-def keypress(key: object = None, delay_ms: int = 0) -> None:
-	if key is None: return
-	from win32api import Sleep
-	keydown(key)
-	Sleep(delay_ms)
-	keyup()
+		keys.append(key)
+		keyboard_controller.press(key)
 
-def typekeys(keys: list | tuple | str | None = None, delay_ms: int = 0) -> None:
-	if keys is None: return
-	for key in keys:
-		keypress(Key.enter if key == "\n" else key, delay_ms)
+	def keyup() -> None: # release most recent key
+		keyboard_controller.release( keys.pop() )
 
-def enter(times: int = 1, delay_ms: int = 0) -> None:
-	for i in range(int(times)):
-		keypress(Key.enter, delay_ms)
+	def keypress(key: object = None, delay_ms: int = 0) -> None:
+		if key is None:
+			return
 
-def keycombo(keys: list | tuple | None = None) -> None:
-	if keys is None: return
-	for key in keys:
 		keydown(key)
-	for key in keys:
+		Sleep(delay_ms)
 		keyup()
 
-def nextwindow(times: int = 1, delay_ms: int = 10) -> None:
-	"""alt tab"""
-	keydown(Key.alt)
-	for i in range(int(times)):
-		keypress(Key.tab, delay_ms)
-	keyup()
+	def typekeys(keys: list | tuple | str | None = None, delay_ms: int = 0) -> None:
+		if keys is None:
+			return
 
-def prevwindow(times: int = 1, delay_ms: int = 10) -> None:
-	"""alt shift tab"""
-	keydown(Key.alt)
-	keydown(Key.shift)
-	for i in range(int(times)):
-		keypress(Key.tab, delay_ms)
-	keyup()
-	keyup()
+		for key in keys:
+			keypress(Key.enter if key == "\n" else key, delay_ms)
 
-def releasekeys() -> None:
-	while keys:
+	def enter(times: int = 1, delay_ms: int = 0) -> None:
+		for i in range(int(times)):
+			keypress(Key.enter, delay_ms)
+
+	def keycombo(keys: list | tuple | None = None, delay_ms: int = 10) -> None:
+		if keys is None:
+			return
+
+		for key in keys:
+			keydown(key)
+
+		Sleep(delay_ms)
+
+		for key in keys:
+			keyup()
+
+	def nextwindow(times: int = 1, delay_ms: int = 10) -> None:
+		"alt tab"
+
+		keydown(Key.alt)
+
+		for i in range(int(times)):
+			keypress(Key.tab, delay_ms)
+
 		keyup()
 
-def releaseall() -> None:
-	releasekeys()
-	releasebuttons()
+	def prevwindow(times: int = 1, delay_ms: int = 10) -> None:
+		"alt shift tab"
+
+		keydown(Key.alt)
+		keydown(Key.shift)
+
+		for i in range(int(times)):
+			keypress(Key.tab, delay_ms)
+
+		keyup()
+		keyup()
+
+	def releasekeys() -> None:
+		while keys:
+			keyup()
+
+	def releaseall() -> None:
+		releasekeys()
+		releasebuttons()
 
 
 
-# media stuff
+	# media stuff
 
-def volumeup(times: int = 1) -> None:
-	for i in range(int(times)):
-		keypress(Key.media_volume_up, 0)
+	def volumeup(times: int = 1) -> None:
+		for i in range(int(times)):
+			keypress(Key.media_volume_up, 0)
 
-def volumedown(times: int = 1) -> None:
-	for i in range(int(times)):
-		keypress(Key.media_volume_down, 0)
+	def volumedown(times: int = 1) -> None:
+		for i in range(int(times)):
+			keypress(Key.media_volume_down, 0)
 
-def pauseplay() -> None:
-	keypress(Key.media_play_pause, 0)
+	def pauseplay() -> None:
+		keypress(Key.media_play_pause, 0)
 
-def togglemute() -> None:
-	keypress(Key.media_volume_mute, 0)
+	def togglemute() -> None:
+		keypress(Key.media_volume_mute, 0)
 
-def nextmedia() -> None:
-	keypress(Key.media_next, 0)
+	def nextmedia() -> None:
+		keypress(Key.media_next, 0)
 
-def prevmedia() -> None:
-	keypress(Key.media_previous, 0)
+	def prevmedia() -> None:
+		keypress(Key.media_previous, 0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 	print_options = {"sep": "\n * ", "end": 3*"\n"}
-	print( "__name__ == '__main__'\nautomate\n\nvariables:\n", *__variables__, **print_options )
-	print( "functions:\n", *__functions__, **print_options )
+	print("__name__ == \"__main__\"\nautomate\n\nvariables:\n", *__variables__, **print_options)
+	print("functions:\n", *__functions__, **print_options )
 	input("Press Enter to exit...\n")
+else:
+	init_module()
+	del init_module
 
-from . import misc
+	typestring = typekeys
+
+	from . import misc
--- Python/automate/misc.py
+++ Python/automate/misc.py
@@ -8,15 +8,15 @@ true = True
 false = False
 ellipsis = type(...) # type(Ellipsis)
 NotImplementedType = type(NotImplemented)
-generator = type(i for i in []) # useless
 function = type(lambda: 1)
 array = list
+generator = type(i for i in []) # useless
 dict_keys = type({}.keys()) # useless
 dict_values = type({}.values()) # useless
 dict_items = type({}.items()) # useless
 realnum = float | int
 number = realnum | complex
-__variables__ = (
+__vars__ = (
 	"Module",
 	"string",
 	"NoneType",
@@ -32,7 +32,6 @@ __variables__ = (
 	"dict_items (useless)",
 	"realnum (float | int)",
 	"number (realnum | complex)",
-	"void",
 	"__variables__",
 	"__functions__",
 	"__all__"
@@ -40,6 +39,7 @@ __variables__ = (
 __functions__ = (
 	"monitors",
 	"view (view module)",
+	"void",
 	"read",
 	"readlines",
 	"write",
@@ -51,31 +51,41 @@ __functions__ = (
 	"crandom (complex random)",
 	"addressOf",
 	"nameOf",
-	"tuplify_complex"
+	"tuplify_complex",
+	"inline_namedtuple",
+	"inline_dataclass"
 )
-__all__ = __variables__ + __functions__
+__all__ = __vars__ + __functions__
 
 # functions
 
 def void(*args, **kwargs) -> None:
+	"takes in any number of arguments and return nothing."
 	pass
 
 def monitors() -> list[dict["top", "left", "width", "height"], ...]:
-	"""returns a list of all connected monitors."""
+	"returns a list of all connected monitors."
+
 	from ctypes.wintypes import RECT
 	from ctypes import (
-		c_int as cint, c_ulong as ul, POINTER as ptr,
-		c_double as dbl, WINFUNCTYPE as cfn, windll
+		c_int as cint,
+		c_ulong as ul,
+		POINTER as ptr,
+		c_double as dbl,
+		WINFUNCTYPE as cfn,
+		windll
 	)
+
 	lst = []
 	windll.user32.EnumDisplayMonitors(0, 0, cfn(cint, ul, ul, ptr(RECT), dbl)(
 		lambda monitor, dc, rect, data: (rct := rect.contents) and lst.append({
-			'top'    : rct.top,
-			'left'   : rct.left,
-			'width'  : rct.right - rct.left,
-			'height' : rct.bottom - rct.top,
+			"top"    : rct.top,
+			"left"   : rct.left,
+			"width"  : rct.right - rct.left,
+			"height" : rct.bottom - rct.top,
 		}) or 1
 	), 0)
+
 	return lst
 
 def view(module: Module, form: int = 8, /, *, form_6_dst = 1) -> tuple[str, ...] | list[str, ...] | set[str, ...] | dict_keys | None:
@@ -122,13 +132,15 @@ def view(module: Module, form: int = 8, /, *, form_6_dst = 1) -> tuple[str, ...]
 		)
 	else: return module
 
-def read(file_loc: str | int, /, *, encoding: str = "utf8") -> str:
+def read(file_loc: str | bytes | int, /, *, encoding: str = "utf8") -> str:
 	"""
 		function for reading from files.
 		takes 1 argument, file path.
 		reads and returns the contents of the file
 	"""
+
 	from io import UnsupportedOperation
+
 	try:
 		with open(file_loc, "r", encoding=encoding) as file:
 			return file.read()
@@ -139,7 +151,7 @@ def read(file_loc: str | int, /, *, encoding: str = "utf8") -> str:
 		print("\nautomate.misc.read() failed to read the file because the file requested is a folder or there are insufficient permissions to view the file.")
 		raise e
 	except TypeError as e:
-		print("\nautomate.misc.read() failed to read the file because it was not a string or non-negative-integer")
+		print("\nautomate.misc.read() failed to read the file because it was not a string, bytes, non-negative-integer, or os.PathLike")
 		raise e
 	except ValueError as e:
 		print("\nautomate.misc.read() failed to read the file because it was not a valid value")
@@ -151,13 +163,14 @@ def read(file_loc: str | int, /, *, encoding: str = "utf8") -> str:
 		print("\nautomate.misc.read() can't read from stdout")
 		raise e
 
-def readlines(file_loc: str | int, /, *, encoding: str = "utf8") -> list[str, ...]:
+def readlines(file_loc: str | bytes  | int, /, *, encoding: str = "utf8") -> list[str, ...]:
 	"""
 		function for reading from files.
 		takes 1 argument, file path.
 		reads and returns the contents of the file split into a list of lines
 	"""
 	from io import UnsupportedOperation
+
 	try:
 		with open(file_loc, "r") as file:
 			return file.readlines()
@@ -168,7 +181,7 @@ def readlines(file_loc: str | int, /, *, encoding: str = "utf8") -> list[str, ..
 		print("\nautomate.misc.readlines() failed to read the file because the file requested is a folder or there are insufficient permissions to view the file.")
 		raise e
 	except TypeError as e:
-		print("\nautomate.misc.readlines() failed to read the file because it was not a string or non-negative integer")
+		print("\nautomate.misc.read() failed to read the file because it was not a string, bytes, non-negative-integer, or os.PathLike")
 		raise e
 	except ValueError as e:
 		print("\nautomate.misc.readlines() failed to read the file because it was not a valid value")
@@ -180,7 +193,7 @@ def readlines(file_loc: str | int, /, *, encoding: str = "utf8") -> list[str, ..
 		print("\nautomate.misc.readlines() can't read from stdout")
 		raise e
 
-def write(file_loc: str | int, text: str = "", form: str = "a", /, *, encoding: str = "utf8") -> None:
+def write(file_loc: str | bytes | int, text: str = "", form: str = "a", /, *, encoding: str = "utf8") -> None:
 	"""
 		function for writing to files.
 		takes 3 arguments:
@@ -197,8 +210,10 @@ def write(file_loc: str | int, text: str = "", form: str = "a", /, *, encoding: 
 		returns None
 	"""
 	from io import UnsupportedOperation
+
 	if form not in {"a", "w", "write", "append"}:
 		form = "a"
+
 	try:
 		with open(file_loc, form[0], encoding=encoding) as file:
 			file.write(text)
@@ -209,7 +224,7 @@ def write(file_loc: str | int, text: str = "", form: str = "a", /, *, encoding: 
 		print("\nautomate.misc.write() failed to write to the file because the file requested is a folder or there are incorrect permissions to view the file.")
 		raise e
 	except TypeError as e:
-		print("\nautomate.misc.write() failed to write to the file was not a string or integer")
+		print("\nautomate.misc.read() failed to read the file because it was not a string, bytes, non-negative-integer, or os.PathLike")
 		raise e
 	except ValueError as e:
 		print("\nautomate.misc.write() failed to write to the file was not a correct value")
@@ -221,7 +236,7 @@ def write(file_loc: str | int, text: str = "", form: str = "a", /, *, encoding: 
 		print("\nautomate.misc.write() can't write to stdin")
 		raise e
 
-def clear(file_loc: str | int, /) -> bool:
+def clear(file_loc: str | bytes | int, /) -> bool:
 	"""
 		takes one argument, the file path.  
 		clears the content of the file.
@@ -234,29 +249,40 @@ def clear(file_loc: str | int, /) -> bool:
 		return false
 	return true
 
-def cmp(num1: realnum = 0, num2: realnum = 0, /) -> int:
+def cmp(num1: realnum = 0, num2: realnum = 0, /) -> -1 | 0 | 1:
 	"""
 		function for comparing floats or integers
+		This is a built-in function in python2, but not python 3 because it is useless
+
 		returns -1 if num1 < num2
 		returns 0 if num1 == num2
 		returns 1 if num1 > num2
 	"""
-	if isinstance(num1, float | int) and isinstance(num2, float | int):
-		return num1 < num2 and -1 or int(num1 != num2)
+
+	if isinstance(num1, realnum) and isinstance(num2, realnum):
+		return -1 if num1 < num2 else int(num1 != num2)
+
 	raise TypeError("automate.misc.cmp() requires an int or float input type")
 
-def ccmp(comp1: number = 0, comp2: number = 0, /) -> tuple[int, int]:
+def ccmp(z: number = 0, w: number = 0, /) -> tuple[int, int] | int:
 	"""
 		returns a tuple of two integers
 		the first integer compares the real parts and the second integer compares the imaginary parts
 	"""
-	if isinstance(comp1, complex | float | int) and isinstance(comp2, complex | float | int):
-		comp1, comp2 = complex(comp1), complex(comp2)
-		return (cmp(comp1.real, comp2.real), cmp(comp1.imag, comp2.imag))
+
+	if isinstance(z, number) and isinstance(w, number):
+		z, w = complex(z), complex(w)
+
+		ret = (cmp(z.real, w.real), cmp(z.imag, w.imag))
+
+		# return a definitive integer comparison value if possible
+		return ret[0] if ret[0] == ret[1] else ret
+
 	raise TypeError("automate.misc.ccmp() requires arguments of type complex, float, or int")
 
 def isntinstance(a, b: type(realnum) | type, /) -> bool:
-	"""return not isinstance(arg1, arg2)"""
+	"return not isinstance(arg1, arg2)"
+
 	return not isinstance(a, b)
 
 def random(return_int: bool = False, /) -> realnum:
@@ -265,17 +291,25 @@ def random(return_int: bool = False, /) -> realnum:
 		more likely to return numbers closer to zero
 		2^-(1 + int(log|x|)) probability that ~|x| will get returned based on its magnitude
 	"""
+
 	from random import randint
+
 	string = "0"
+
 	while randint(0, 1):
 		string += str(randint(0, 9))
+
 	if not return_int:
 		string += "."
-		while 1:
+
+		while True:
 			string += str(randint(0, 9))
+
 			if randint(0, 1):
 				break
+
 	ans = float(string) * (randint(0, 1) or -1)
+
 	return int(ans) if return_int else ans
 
 def crandom(return_int: bool = False, /) -> complex:
@@ -284,6 +318,7 @@ def crandom(return_int: bool = False, /) -> complex:
 		and complex parts can be in (-∞, ∞)
 		more likely to return numbers closer to zero.
 	"""
+
 	return complex(random(return_int), random(return_int))
 
 def addressOf(fn: function, form: type = int, /) -> str | int:
@@ -291,14 +326,18 @@ def addressOf(fn: function, form: type = int, /) -> str | int:
 		function for getting the address of functions
 		takes 1 function argument and returns, as a string, the address
 	"""
-	# function cannot be a base class, thus "isinstance" is not required here
-	if type(fn) is type(lambda: 1):
+
+	# function can't be extended, so isinstance isn't required
+	if isinstance(fn, function):
 		from re import sub
+
 		ans = sub("(.* ){3}", "", str(fn))[:-1]
+
 		if form is str:
 			return ans
 		elif form is int:
 			return int(ans, 16)
+
 	raise TypeError("automate.misc.addressOf() requires a function input")
 
 def nameOf(fn: function, /) -> str:
@@ -306,10 +345,13 @@ def nameOf(fn: function, /) -> str:
 		function for getting the name of functions
 		takes 1 function argument and returns, as a string, the name
 	"""
-	# function cannot be a base class, thus "isinstance" is not required here
-	if type(fn) is type(lambda: 1):
+
+	# function can't be extended, so isinstance isn't required
+	if isinstance(fn, function):
 		from re import sub
+
 		return sub("(<function )| .*", "", str(fn))
+
 	raise TypeError("automate.misc.nameOf() requires a function input")
 
 def tuplify_complex(number: complex = complex(), /) -> tuple[float, float]:
@@ -317,10 +359,29 @@ def tuplify_complex(number: complex = complex(), /) -> tuple[float, float]:
 		returns any complex number in the form of a tuple.
 		returns (input.real, input.imag)
 	"""
+
 	return (number.real, number.imag)
 
-if __name__ == '__main__':
+def inline_namedtuple(name: str | None = None, /, **kwargs):
+	from collections import namedtuple as _namedtuple
+
+	if name is None:
+		name = "CustomTuple"
+
+	return _namedtuple(name, kwargs.keys()) (*kwargs.values())
+
+def inline_dataclass(name: str | None = None, types: tuple | list | None = None, /, **kwargs):
+	from dataclasses import make_dataclass
+
+	if name is None:
+		name = "CustomDataclass"
+
+	keys = kwargs.keys()
+
+	return make_dataclass(name, keys if types is None else zip(keys, types))(**kwargs)
+
+if __name__ == "__main__":
 	print_options = {"sep": "\n * ", "end": 3*"\n"}
-	print( "__name__ == '__main__'\nautomate.misc\n\nvariables:\n", *__variables__, **print_options )
+	print( "__name__ == \"__main__\"\nautomate.misc\n\nvariables:\n", *__variables__, **print_options )
 	print( "functions:\n", *__functions__, **print_options )
 	input("Press Enter to exit...\n")
