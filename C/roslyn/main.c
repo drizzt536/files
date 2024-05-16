b6228af07e21d2ba3746a53b8e80bd85e02bd513
Daniel Janusch <djanusch2007@gmail.com>
2024-05-16 00:52:22 -0700

    add roslyn files

---
+++ C/roslyn/main.c
@@ -0,0 +1,34 @@
+// assumes that the .NET SDK thing is installed: dotnet.microsoft.com/download
+// I don't want to download Visual Studio or just do `dotnet csc.dll [args]`
+
+#include "D:/ExtF/CodeFiles/C/string-join.h"
+
+#ifndef ROOT
+	#error "`-D ROOT=[dotnet root directory]` must be given to gcc for main.c"
+#endif
+
+#ifndef VERSION
+	#error "`-D VERSION=[version]` must be given to gcc for main.c"
+#endif
+
+#ifndef LANG
+	#error "`-D LANG=[csc|vbc]` must be given to gcc for main.c"
+#endif
+
+
+#define DOTNET	ROOT "dotnet.exe\""
+#define DLL		ROOT "sdk/" VERSION "/Roslyn/bincore/" LANG ".dll\""
+
+int main(int argc, char **argv) {
+
+	const string args = strjoin(++argv, --argc);
+
+	char *const cmd = malloc(strlen(DOTNET) + strlen(DLL) + args.l + 5);
+
+	OUT_OF_MEMORY(cmd, 3);
+
+	sprintf(cmd, "\"%s %s %s\"", DOTNET, DLL, args.s);
+
+	return system(cmd);
+	// `free(args.s)` and `free(cmd)` happen automatically by the OS
+}
