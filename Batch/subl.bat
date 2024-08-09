a4010d5c8d099acd44e5eadee2ac53bdbe18e422
Daniel Janusch <djanusch2007@gmail.com>
2024-08-09 15:08:52 -0700

    update Batch/subl.bat
    suppress messages to stderr.

--- Batch/subl.bat
+++ Batch/subl.bat
@@ -4,16 +4,16 @@ if exist "./whereis.exe" ( "./whereis.exe" subl.exe > nul
 ) else if exist C:/Windows/System32/where.exe ( C:/Windows/System32/where.exe subl.exe > nul
 ) else ( echo `where.exe` and `whereis.exe` cannot be found.
 
-	subl.exe %*
+	subl.exe %* 2>nul
 	if %ErrorLevel% equ 0 (exit 0) else set ErrorLevel=0
 
-	"C:/Program Files/Sublime Text/subl.exe" %*
+	"C:/Program Files/Sublime Text/subl.exe" %* 2>nul
 	if %ErrorLevel% equ 0 (exit 0) else set ErrorLevel=0
 
-	"../Sublime Text/subl.exe" %*
+	"../Sublime Text/subl.exe" %* 2>nul
 	if %ErrorLevel% equ 0 (exit 0) else set ErrorLevel=0
 
-	"%~d0/ExtF/Files/Sublime Text/subl.exe" %*
+	"%~d0/ExtF/Files/Sublime Text/subl.exe" %* 2>nul
 	if %ErrorLevel% equ 0 exit 0
 
 	echo `subl.exe` could not be found
@@ -21,10 +21,10 @@ if exist "./whereis.exe" ( "./whereis.exe" subl.exe > nul
 )
 
 
-if %ErrorLevel% equ 0 ( subl.exe %*
-) else if exist "C:/Program Files/Sublime Text/subl.exe" ( "C:/Program Files/Sublime Text/subl.exe" %*
-) else if exist "../Sublime Text/subl.exe" ( "../Sublime Text/subl.exe" %*
-) else if exist "%~d0/ExtF/Files/Sublime Text/subl.exe" ( "%~d0/ExtF/Files/Sublime Text/subl.exe" %*
+if %ErrorLevel% equ 0 ( subl.exe %* 2>nul
+) else if exist "C:/Program Files/Sublime Text/subl.exe" ( "C:/Program Files/Sublime Text/subl.exe" %* 2>nul
+) else if exist "../Sublime Text/subl.exe" ( "../Sublime Text/subl.exe" %* 2>nul
+) else if exist "%~d0/ExtF/Files/Sublime Text/subl.exe" ( "%~d0/ExtF/Files/Sublime Text/subl.exe" %* 2>nul
 ) else (
 	echo subl.exe not found
 	exit 2
