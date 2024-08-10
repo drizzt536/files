99e27da8cb64917dcd64111d56c76cb1c6825700
Daniel Janusch <djanusch2007@gmail.com>
2024-08-10 00:18:46 -0700

    update C/string-join.h
     - remove ending underscore from header guard symbol
     - make as many variables as possible constant. This should allow for usage in more contexts since you can cast more `const`s, but can't cast them away.
     - rename `strjoinc` arguments to `argc` and `argv` like `strjoin`.

--- C/string-join.h
+++ C/string-join.h
@@ -1,5 +1,5 @@
-#ifndef STRING_JOIN_H_
-	#define STRING_JOIN_H_
+#ifndef STRING_JOIN_H
+	#define STRING_JOIN_H
 
 	// OUT_OF_MEMORY,
 	// stdlib.h
@@ -14,13 +14,13 @@
 		size_t l; // length
 	} string;
 
-	string strjoinc(size_t num, char *strs[], const char joiner) {
+	string strjoinc(const size_t argc, const char *const *const argv, const char joiner) {
 		// join strings with a space. returns a pointer to the heap.
 		// example: `const string args = strjoinc(--argc, ++argv, ' ');`
 
 		char *res;
 
-		if (num == 0) {
+		if (argc == 0) {
 			// O(1)
 			res = malloc(1);
 			*res = '\0';
@@ -29,19 +29,12 @@
 			return (string) {res, 0};
 		}
 
-		/*if (num == 1) {
-			res = strdup(*strs);
-			OUT_OF_MEMORY(res, 2);
-
-			return (string) {res, strlen(res)};
-		}*/
-
 		// O(2n), where `n` is the combined length of the strings (without joining spaces)
-		size_t i = num;
-		size_t length = num; // room for spaces between args, and null byte
+		size_t i = argc;
+		size_t length = argc; // room for spaces between args, and null byte
 
 		while (i --> 0)
-			length += strlen(strs[i]);
+			length += strlen(argv[i]);
 
 		res = malloc(length);
 
@@ -49,21 +42,21 @@
 
 		i = 0;
 
-		char *current = strs[0];
+		const char *current = argv[0];
 
-		// `strcpy(res, *strs)` and `i = strlen(*strs)` at the same time
+		// `strcpy(res, *argv)` and `i = strlen(*argv)` at the same time
 		for (; current[i]; i++)
 			res[i] = current[i];
 
-		for (size_t n = 1, iPrevious = 0; n < num; n++) {
+		for (size_t n = 1, iPrevious = 0; n < argc; n++) {
 			// if there are no more strings, break
 
 			res[iPrevious + i] = joiner;
-			current = strs[n];
+			current = argv[n];
 			iPrevious += i + 1;
 
-			// strcat(res, strs[n]) without having to find the end of `res` first.
-			// O( strlen(strs[n]) ).
+			// strcat(res, argv[n]) without having to find the end of `res` first.
+			// O( strlen(argv[n]) ).
 			for (i = 0; current[i]; i++)
 				res[iPrevious + i] = current[i];
 		}
@@ -73,7 +66,7 @@
 		return (string) {res, length - 1};
 	}
 
-	string strjoin(size_t argc, char *argv[]) {
+	string strjoin(const size_t argc, const char *const *const argv) {
 		return strjoinc(argc, argv, ' ');
 	}
 
