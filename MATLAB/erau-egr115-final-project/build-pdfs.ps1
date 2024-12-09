a347e2a54ade98000af87e78509491fb1b521499
Daniel Janusch <djanusch2007@gmail.com>
2024-12-09 09:45:17 -0700

    reformat build-pdfs.ps1
    - switch from spaces to tabs. I don't know why it was spaces in the first place.
    - change from `silent` to `SilentlyContinue` since that is the actual name of the argument

--- MATLAB/erau-egr115-final-project/build-pdfs.ps1
+++ MATLAB/erau-egr115-final-project/build-pdfs.ps1
@@ -1,15 +1,15 @@
 # convert the SVGs to PDF for inclusion in LaTeX PDFs.
 
-if (!(gcm inkscape -type app -ErrorAction silent)) {
-    throw "inkscape is not installed and is required for converting SVG to PDF"
-    # there is also rsvg-convert and CairoSVG, but rsvg-convert is less reliable,
-    # and CairoSVG doesn't work at all with masks and is incredibly slow and deprecated.
+if (!(gcm inkscape -type app -ErrorAction SilentlyContinue)) {
+	throw "inkscape is not installed and is required for converting SVG to PDF"
+	# there is also rsvg-convert and CairoSVG, but rsvg-convert is less reliable,
+	# and CairoSVG doesn't work at all with masks and is incredibly slow and deprecated.
 }
 
 ls ./svg | % name | % {
-    $cmd = "inkscape ./svg/$_ --export-type=pdf " +
-        "--export-filename=./pdf/$($_ -replace ".svg", ".pdf")"
+	$cmd = "inkscape ./svg/$_ --export-type=pdf " +
+		"--export-filename=./pdf/$($_ -replace ".svg", ".pdf")"
 
-    write-host $cmd
-    iex $cmd
+	write-host $cmd
+	iex $cmd
 }
