95ad4b6ebb956c148c1e19d57d83f5dfbd996b72
Daniel Janusch <djanusch2007@gmail.com>
2025-01-25 17:51:52 -0700

    add JavaScript/completely normal code.js
    it is completely normal code that runs without errors in every JavaScript environment that doesn't use strict mode by default.

---
+++ JavaScript/completely normal code.js
@@ -0,0 +1,33 @@
+!function crash_Browser_Question_Mark_Question_Mark() { // no other scripts should execute before this.
+	const {x} = {...{ x: {x: 15} }};
+	with (new class {}) with (x) with (x)
+	console.log("%o%o", toString(x) + [1] + [2] + [3] );
+	console.log("%o", [toString(x), <!-- 325.6]);
+	arguments[0] ?? null]);
+	if (arguments.callee)
+	for (var i = 10, arr = [...[...[(() => {})()],],]; i --> 0 ; undefined, ({...globalThis.globalThis}),4) ;{
+		--> for (let j = 10; j --> 0 ; k -= j + i >> k & 0b11;) {
+			--> for (k = 0; k < j ; j --> k --> i <-- j) {
+				--> console.log(i) >> j < k + "
+				console.log(i) + i --> i;
+				i &= 0o1123 ?? false
+					?-+
+					2 + 0x12f << 2 & 0b11111111 :
+					i ^ k ** ([...arr*6]["b"+"a"+ +"a"+"a", j + orange] ?? 0);
+				i --> (i += 2) + 1, arr.push(i), WebAssembly ?.
+				--> p :: Int -> Int -> Int = \x y -> x `mod y
+				<!-- for (let p of arr) console.log(-p--<-++i+++k)
+				NaN === NaN && console.log(1434) || console.log(-+ +console.log(41)) // 1434\
+			}
+		--> }
+		arr.push(
+			arr.pop(
+				13, 14 + (Math.max() < Math.min())
+			)?.length ?? arr[0] ?? i
+		);
+	--> } <!-- ????
+
+	while (true) return arr.
+	pop(), [arr][+!+[]];
+}(($=$$=>`$=${$},$($)`,$($)));
+/* `; console.log("\32") */ // console.log(/[^e-z1-4]{,3}?(?:3)/flag.test("asdf"))'use strict
