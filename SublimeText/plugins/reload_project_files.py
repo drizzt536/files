5335474f0f069ae320f92c9829c4df87ae125a07
Daniel Janusch <djanusch2007@gmail.com>
2026-01-02 19:49:41 -0700

    remove `SublimeText/plugins/reload_project_files.py`
    I found out there is an actual command for this: `Project: Refresh Folders`.

--- SublimeText/plugins/reload_project_files.py
+++
@@ -1,9 +0,0 @@
-from sublime_plugin import WindowCommand
-
-class ReloadProjectFiles(WindowCommand):
-	def run(self):
-		proj_data = self.window.project_data()
-
-		if proj_data:
-			self.window.set_project_data({})
-			self.window.set_project_data(proj_data)
